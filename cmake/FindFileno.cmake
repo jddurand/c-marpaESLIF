b02da2007028a213c5eb4379af87feb81b47c610
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-04-03 12:40:07 +0200

    cmake/Find*.cmake: use StringToInternalBool

--- cmake/FindFileno.cmake
+++ cmake/FindFileno.cmake
@@ -1,9 +1,17 @@
+INCLUDE (StringToInternalBool)
 MACRO (FINDFILENO)
+  #
+  # Dependencies
+  #
+  STRINGTOINTERNALBOOL(_HAVE_STDIO_H HAVE_STDIO_H)
+  #
+  # Test
+  #
   FOREACH (KEYWORD "fileno" "_fileno" "__fileno")
     MESSAGE(STATUS "Looking for ${KEYWORD}")
     TRY_COMPILE (C_HAS_${KEYWORD} ${CMAKE_CURRENT_BINARY_DIR}
       ${CMAKE_CURRENT_SOURCE_DIR}/cmake/fileno.c
-      COMPILE_DEFINITIONS "-DC_FILENO=${KEYWORD} -DHAVE_STDIO_H=${HAVE_STDIO_H}")
+      COMPILE_DEFINITIONS "-DC_FILENO=${KEYWORD} -DHAVE_STDIO_H=${_HAVE_STDIO_H}")
     IF (C_HAS_${KEYWORD})
       MESSAGE(STATUS "Looking for ${KEYWORD} - found")
       SET (C_FILENO ${KEYWORD})
