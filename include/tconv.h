d2ab5ab560824e5a9a4913c4c493cdb97582df0d
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-04-19 08:24:00 +0200

    dev+

--- include/tconv.h
+++ include/tconv.h
@@ -1,6 +1,11 @@
 #ifndef TCONV_H
 #define TCONV_H
 
+#include <stddef.h>
+#include <genericLogger.h>
+
+#include "tconv_export.h"
+
 typedef struct tconv *tconv_t;
 
 /************************************************************************************/
@@ -11,83 +16,94 @@ typedef struct tconv *tconv_t;
 /* This is why it is recommended to only use charsets in tconv_open(), and to use   */
 /* tconv_open_ext() if you want to have further configuration.                      */
 /************************************************************************************/
-tconv_t  tconv_open(const char *tocode, const char *fromcode);
-size_t   tconv(tconv_t cd, char **inbuf, size_t *inbytesleft, char **outbuf, size_t *outbytesleft);
-int      tconv_close(tconv_t cd);
+TCONV_EXPORT tconv_t  tconv_open(const char *tocodes, const char *fromcodes);
+TCONV_EXPORT size_t   tconv(tconv_t tconvp, char **inbufsp, size_t *inbytesleftlp, char **outbufsp, size_t *outbytesleftlp);
+TCONV_EXPORT int      tconv_close(tconv_t tconvp);
 
 /****************************************************/
 /* Extended version to handle backend specificities */
 /****************************************************/
 
-/* ------------------- */
-/* Cconversion options */
-/* ------------------- */
-
-typedef struct tconv_convert_iconv {    /* Built-in */
-  short translitb;                 /* Default:    0 */
-  short ignoreb;                   /* Default:    0 */
-  size_t bufsizel;                 /* Default:    0 (no memory limit) */
-} tconv_convert_iconv_t;
-
-typedef struct tconv_convert_ICU {   /* Built-in */
-  void *fromCallackp;           /* Default: NULL */
-  void *fromFallbackp;          /* Default: NULL */
-  void *toCallackp;             /* Default: NULL */
-  void *toFallbackp;            /* Default: NULL */
-  size_t bufsizel;              /* Default:    0 (no memory limit) */
-} tconv_convert_ICU_t;
-
-typedef struct tconv_convert_external {     /* External */
-  void    *optionp;
-  void    *(*tconv_convert_open)(const char *tocode, const char *fromcode, void *optionp);
-  size_t   (*tconv_convert)(void *contextp, char **inbuf, size_t *inbytesleft, char **outbuf, size_t *outbytesleft);
-  int      (*tconv_convert_close)(void *contextp);
-} tconv_convert_external_t;
-
-typedef struct tconv_convert_plugin {     /* plugin */
-  void    *optionp;
-  char    *filenames;
-} tconv_convert_plugin_t;
-
-typedef struct tconv_convert {
-  enum {
-    TCONV_CONVERT_ICONV,
-    TCONV_CONVERT_ICU,
-    TCONV_CONVERT_EXTERNAL,
-    TCONV_CONVERT_PLUGIN
-  } converteri;
-  union {
-    tconv_convert_iconv_t    *iconvp;
-    tconv_convert_ICU        *ICUp;
-    tconv_convert_external_t *externalp;
-  } u;
-} tconv_convert_t;
-
 /* ------------------------- */
 /* Charset detection options */
 /* ------------------------- */
-
-typedef struct tconv_charset_cchardet { /*      Built-in */
-  float confidencef;                    /* Default: 0.1f */
-} tconv_charset_cchardet_t;
 
 typedef struct tconv_charset_ICU { /*    Built-in */
   int confidencei;                 /* Default: 10 */
 } tconv_charset_ICU_t;
 
+typedef struct tconv_charset_cchardet { /*      Built-in */
+  float confidencef;                    /* Default: 0.1f */
+} tconv_charset_cchardet_t;
+
 typedef struct tconv_charset_external {     /* External */
-  void *optionp;
-  void *(*tconv_charset_new)(void *optionp);
-  char *(*tconv_charset)(void *contextp, char *bytep, size_t bytel);
-  void  (*tconv_charset_free)(void *contextp);
+  void *optionp; /* Default: NULL */
+  void *(*tconv_charset_new)(void *optionp); /* Default: NULL */
+  char *(*tconv_charset)(void *contextp, char *bytep, size_t bytel); /* Default: NULL */
+  void  (*tconv_charset_free)(void *contextp); /* Default: NULL */
 } tconv_charset_external_t;
 
+typedef struct tconv_charset_plugin {     /* plugin */
+  void    *optionp;   /* Default: NULL */
+  char    *filenames; /* Default: NULL */
+} tconv_charset_plugin_t;
+
 typedef struct tconv_charset {
-  enum { TCONV_CHARSET_CCHARDET, TCONV_CHARSET_ICU, TCONV_CHARSET_X } detectori;
+  enum { /* Default: ICU if found, else CCHARDET, else -1 */
+    TCONV_CHARSET_ICU,
+    TCONV_CHARSET_CCHARDET,
+    TCONV_CHARSET_X
+  } detectori;
   union {
-    tconv_charset_cchardet_t  *cchardetp;
-    tconv_charset_ICU_t       *ICUp;
-    tconv_charset_external_t  *externalp;
+    tconv_charset_ICU_t       ICU;
+    tconv_charset_cchardet_t  cchardet;
+    tconv_charset_external_t  external;
+    tconv_charset_plugin_t    plugin;
+  } u;
+} tconv_charset_t;
+
+/* ------------------ */
+/* conversion options */
+/* ------------------ */
+
+typedef struct tconv_convert_ICU {   /* Built-in */
+  size_t bufSizel;              /* Default:    0 (no memory limit) */
+  void *fromCallbackp;          /* Default: NULL */
+  void *fromFallbackp;          /* Default: NULL */
+  void *toCallbackp;            /* Default: NULL */
+  void *toFallbackp;            /* Default: NULL */
+} tconv_convert_ICU_t;
+
+typedef struct tconv_convert_iconv {    /* Built-in */
+  size_t bufSizel;                 /* Default:    0 (no memory limit) */
+  short translitb;                 /* Default:    0 */
+  short ignoreb;                   /* Default:    0 */
+} tconv_convert_iconv_t;
+
+typedef struct tconv_convert_external {     /* External */
+  void    *optionp;  /* Default: NULL */
+  void    *(*tconv_convert_open)(const char *tocode, const char *fromcode, void *optionp); /* Default: NULL */
+  size_t   (*tconv_convert)(void *contextp, char **inbuf, size_t *inbytesleft, char **outbuf, size_t *outbytesleft); /* Default: NULL */
+  int      (*tconv_convert_close)(void *contextp); /* Default: NULL */
+} tconv_convert_external_t;
+
+typedef struct tconv_convert_plugin {     /* plugin */
+  void    *optionp;   /* Default: NULL */
+  char    *filenames; /* Default: NULL */
+} tconv_convert_plugin_t;
+
+typedef struct tconv_convert {
+  enum { /* Default: ICU if found, else ICONV if found, else -1 */
+    TCONV_CONVERT_ICU = 0,
+    TCONV_CONVERT_ICONV,
+    TCONV_CONVERT_EXTERNAL,
+    TCONV_CONVERT_PLUGIN,
+  } converteri;
+  union {
+    tconv_convert_ICU_t      ICU;
+    tconv_convert_iconv_t    iconv;
+    tconv_convert_external_t external;
+    tconv_convert_plugin_t   plugin;
   } u;
 } tconv_convert_t;
 
@@ -95,10 +111,12 @@ typedef struct tconv_charset {
 /* Global options */
 /* -------------- */
 typedef struct tconv_opt {
-  tconv_convert_t *convertp; /* Default: NULL */
-  tconv_charset_t *charsetp; /* Default: NULL */
+  genericLogger_t *genericLoggerp; /* Default: NULL - will always log unless -DTCONV_NTRACE */
+  tconv_charset_t  charset;
+  tconv_convert_t  convert;
 } tconv_opt_t;
 
-tconv_t  tconv_open_ext(const char *tocode, const char *fromcode, tconv_opt_t *opt);
+TCONV_EXPORT tconv_opt_t *tconv_opt_default();
+TCONV_EXPORT tconv_t      tconv_open_ext(const char *tocodes, const char *fromcodes, tconv_opt_t *optp);
 
 #endif /* TCONV_H */
