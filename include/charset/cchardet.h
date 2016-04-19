6900af1e04744c7d157092a7e158a795b4cba78a
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-04-19 16:00:52 +0200

    dev+

---
+++ include/charset/cchardet.h
@@ -0,0 +1,14 @@
+#ifndef TCONV_CHARSET_CCHARDET_H
+#define TCONV_CHARSET_CCHARDET_H
+
+#include <stddef.h>
+
+typedef struct tconv_charset_cchardet { /*      Built-in */
+  float confidencef;                    /* Default: 0.1f */
+} tconv_charset_cchardet_t;
+
+TCONV_NO_EXPORT void *tconv_charset_cchardet_new(void *optionp);
+TCONV_NO_EXPORT char *tconv_charset_cchardet_run(void *contextp, char *bytep, size_t bytel);
+TCONV_NO_EXPORT void  tconv_charset_cchardet_free(void *contextp);
+
+#endif /*  TCONV_CHARSET_CCHARDET_H */
