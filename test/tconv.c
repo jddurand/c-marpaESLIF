8ceda06c4ceb82e2a69d6c65e09f476a1d32fd05
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-04-19 11:50:43 +0200

    dev+

--- test/tconv.c
+++ test/tconv.c
@@ -1,8 +1,12 @@
+#include <stdlib.h>
 #include "tconv.h"
 
 int main(int argc, char **argv)
 {
-  tconv_open(NULL, NULL);
+  tconv_t tconvp;
+
+  tconvp = tconv_open(NULL, NULL);
+  tconv_close(tconvp);
 
   return 0;
 }
