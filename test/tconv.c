52ac6c2482fa1a9b0bf2ad4d9e7da23009dacc1a
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-04-20 15:07:29 +0200

    test+

--- test/tconv.c
+++ test/tconv.c
@@ -1,12 +1,28 @@
 #include <stdlib.h>
 #include "tconv.h"
+#include "genericLogger.h"
 
 int main(int argc, char **argv)
 {
   tconv_t tconvp;
+  tconv_charset_t tconvCharset;
+  tconv_option_t tconvOption;
+
+
+  tconvCharset.charseti = TCONV_CONVERT_PLUGIN;
+  tconvCharset.u.plugin.filenames = "TOTO";
+
+  tconvOption.charsetp = &tconvCharset;
+  tconvOption.convertp = NULL;
+  tconvOption.genericLoggerp = GENERICLOGGER_NEW(GENERICLOGGER_LOGLEVEL_TRACE);
 
   tconvp = tconv_open(NULL, NULL);
   tconv_close(tconvp);
+
+  tconvp = tconv_open_ext(NULL, NULL, &tconvOption);
+  tconv_close(tconvp);
+
+  GENERICLOGGER_FREE(tconvOption.genericLoggerp);
 
   return 0;
 }
