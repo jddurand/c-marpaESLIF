9b3a7b872b81e79b3294acd041e8778206ef6381
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-04-22 17:27:54 +0200

    convert/iconv.c: reduce it to the minimum

--- src/tconv/convert/iconv.c
+++ src/tconv/convert/iconv.c
@@ -5,59 +5,28 @@
 
 #include "tconv/convert/iconv.h"
 
+/***********************************************************************/
+/* Our semantics are the iconv ones - so naturally the iconv interface */
+/* should do nothing else but call iconv -;                            */
+/***********************************************************************/
+
 /*****************************************************************************/
 void  *tconv_convert_iconv_new(tconv_t tconvp, const char *tocodes, const char *fromcodes, void *voidp)
 /*****************************************************************************/
 {
-  iconv_t                       iconvp;
-  
-  if ((tocodes == NULL) || (fromcodes == NULL)) {
-    goto err;
-  }
-
-  iconvp = iconv_open(tocodes, fromcodes);
-  if (iconvp == NULL) {
-    goto err;
-  }
-
-  return iconvp;
-  
- err:
-  errno = EINVAL;
-  return NULL;
+  return iconv_open(tocodes, fromcodes);
 }
 
 /*****************************************************************************/
 size_t tconv_convert_iconv_run(tconv_t tconvp, void *contextp, char **inbufsp, size_t *inbytesleftlp, char **outbufsp, size_t *outbytesleftlp)
 /*****************************************************************************/
 {
-  iconv_t iconvp = (iconv_t) contextp;
-
-  if (iconvp == NULL) {
-    goto err;
-  }
-
-  return iconv(iconvp, inbufsp, inbytesleftlp, outbufsp, outbytesleftlp);
-
- err:
-  errno = EINVAL;
-  return (size_t)-1;
+  return iconv((iconv_t) contextp, inbufsp, inbytesleftlp, outbufsp, outbytesleftlp);
 }
 
 /*****************************************************************************/
 int tconv_convert_iconv_free(tconv_t tconvp, void *contextp)
 /*****************************************************************************/
 {
-  iconv_t iconvp = (iconv_t) contextp;
-
-  if (iconvp == NULL) {
-    goto err;
-  }
-
-  return iconv_close(iconvp);
-
- err:
-  errno = EINVAL;
-  return -1;
+  return iconv_close((iconv_t) contextp);
 }
-
