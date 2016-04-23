17eee0a51125df7bc45e0d44c98615ec2e498e22
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-04-23 23:03:12 +0200

    t+

--- test/tconv.c
+++ test/tconv.c
@@ -7,18 +7,17 @@
 
 void _trace(void *userDatavp, const char *msgs);
 
+void _loop(tconv_t tconvp, char *inbufs, size_t inbytesleftl, char *outbufs, size_t outbytesleftl);
+
 int main(int argc, char **argv)
 {
   tconv_t tconvp;
   tconv_charset_t tconvCharset;
   tconv_option_t tconvOption;
-  char  *inputs;
-  size_t inputl;
-  char  *outputs;
-  size_t outputl;
-  char  *origInputs;
-  char  *endInputs;
-  char  *origOutputs;
+  char   *inputs;
+  size_t  inputl;
+  char   *outputs;
+  size_t  outputl;
 
   tconvOption.charsetp       = &tconvCharset;
   tconvOption.convertp       = NULL;
@@ -26,44 +25,44 @@ int main(int argc, char **argv)
 
   tconvCharset.charseti = TCONV_CHARSET_ICU;
   tconvCharset.u.ICUOptionp = NULL;
-  tconvp = tconv_open_ext(NULL, NULL, &tconvOption);
+  tconvp = tconv_open_ext("ISO-8859-1", NULL, &tconvOption);
   tconv_trace_on(tconvp);
-  
-  inputs = "My Input";
-  origInputs = inputs;
+  inputs = "ßàéöø";
   inputl = strlen(inputs) + 1;
-  endInputs = inputs + inputl;
-  outputs = "                             ";
-  outputl = strlen(outputs) + 1;
-  origOutputs = outputs;
-  while (inputs < endInputs) {
-    tconv(tconvp, &inputs, &inputl, &outputs, &outputl);
-  }
+  outputs = malloc(200);
+  outputl = 200;
+  _loop(tconvp, inputs, inputl, outputs, outputl);
   tconv_close(tconvp);
-  printf("\"%s\" => \"%s\"\n", origInputs, origOutputs);
+  printf("\"%s\" => \"%s\"\n", inputs, outputs);
+  free(outputs);
 
   tconvCharset.charseti = TCONV_CHARSET_CCHARDET;
   tconvCharset.u.cchardetOptionp = NULL;
-  tconvp = tconv_open_ext(NULL, NULL, &tconvOption);
+  tconvp = tconv_open_ext("ISO-8859-1", NULL, &tconvOption);
   tconv_trace_on(tconvp);
-  
-  inputs = "My Input";
-  origInputs = inputs;
+  inputs = "ßàéöø";
   inputl = strlen(inputs) + 1;
-  endInputs = inputs + inputl;
-  outputs = "                             ";
-  outputl = strlen(outputs) + 1;
-  origOutputs = outputs;
-  while (inputs < endInputs) {
-    tconv(tconvp, &inputs, &inputl, &outputs, &outputl);
-  }
+  outputs = malloc(200);
+  outputl = 200;
+  _loop(tconvp, inputs, inputl, outputs, outputl);
   tconv_close(tconvp);
-  printf("\"%s\" => \"%s\"\n", origInputs, origOutputs);
+  printf("\"%s\" => \"%s\"\n", inputs, outputs);
+  free(outputs);
 
   return 0;
 }
 
-void _trace(void *userDatavp, const char *msgs) {
+void _trace(void *userDatavp, const char *msgs)
+{
   GENERICLOGGER_TRACE(NULL, msgs);
+}
+
+void _loop(tconv_t tconvp, char *inbufs, size_t inbytesleftl, char *outbufs, size_t outbytesleftl)
+{
+  while (inbytesleftl > 0) {
+    if (tconv(tconvp, &inbufs, &inbytesleftl, &outbufs, &outbytesleftl) == (size_t)-1) {
+      break;
+    }
+  }
 }
 
