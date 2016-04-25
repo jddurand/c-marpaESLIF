dec428c097bf344b0eec716eb4a1ce0ad7858b36
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-04-25 14:29:20 +0200

    ICU dev+

--- include/tconv/convert/ICU.h
+++ include/tconv/convert/ICU.h
@@ -3,17 +3,15 @@
 
 #include <stddef.h>
 
-#include "tconv/export.h"
-
-typedef struct tconv_convert_ICU_option {
-  int confidencei;                    /* Default: 10 */
-} tconv_convert_ICU_option_t;
+typedef struct tconv_convert_icu_option {
+  size_t uCharSizel;           /* Size of intermediary buffer. Default: 1024*1024 */
+} tconv_convert_icu_option_t;
 
 #include "tconv.h"
 #include "tconv/export.h"
 
-TCONV_NO_EXPORT void   *tconv_convert_ICU_new (tconv_t tconvp, const char *tocodes, const char *fromcodes, void *optionp);
-TCONV_NO_EXPORT size_t  tconv_convert_ICU_run (tconv_t tconvp, void *contextp, char **inbufsp, size_t *inbytesleftlp, char **outbufsp, size_t *outbytesleftlp);
-TCONV_NO_EXPORT int     tconv_convert_ICU_free(tconv_t tconvp, void *contextp);
+TCONV_NO_EXPORT void  *tconv_convert_ICU_new (tconv_t tconv, const char *tocodes, const char *fromcodes, void *optionp);
+TCONV_NO_EXPORT size_t tconv_convert_ICU_run (tconv_t tconv, void *contextp, char **inbufsp, size_t *inbytesleftlp, char **outbufsp, size_t *outbytesleftlp);
+TCONV_NO_EXPORT int    tconv_convert_ICU_free(tconv_t tconv, void *contextp);
 
 #endif /*  TCONV_CONVERT_ICU_H */
