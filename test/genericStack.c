db33c39debb0664955cdf9bfd3700697618eb25a
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-05-11 12:02:56 +0200

    t+

---
+++ test/genericStack.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "genericStack.h"
+
+void myFunction(int arg1, GENERICSTACK_DECL(myStack), int argn);
+
+int main() {
+  GENERICSTACK_DECL(myStack);
+
+  GENERICSTACK_NEW(myStack);
+  GENERICSTACK_PUSH_INT(myStack, 1);
+  GENERICSTACK_PUSH_FLOAT(myStack, 2);
+
+  myFunction(-1, myStack, 999);
+}
+
+void myFunction(int arg1, GENERICSTACK_DECL(myStack), int argn) {
+  printf("arg1: %d\n", arg1);
+  printf("float: %f\n", GENERICSTACK_POP_FLOAT(myStack));
+  printf("int: %d\n", GENERICSTACK_POP_INT(myStack));
+  printf("argn: %d\n", argn);
+
+  GENERICSTACK_FREE(myStack);
+}
+
