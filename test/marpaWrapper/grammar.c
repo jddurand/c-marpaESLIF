52e92c552839c58f6bcd24952fd7a74b835cf375
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-05-27 15:52:11 +0200

    dev+

--- test/marpaWrapper/grammar.c
+++ test/marpaWrapper/grammar.c
@@ -4,10 +4,13 @@
 
 int main(int argc, char **argv) {
   marpaWrapperGrammar_t *marpaWrapperGrammarp;
+  marpaWrapperGrammar_t *marpaWrapperGrammarClonep;
 
-  marpaWrapperGrammarp = marpaWrapperGrammar_newp(NULL);
+  marpaWrapperGrammarp      = marpaWrapperGrammar_newp(NULL);
+  marpaWrapperGrammarClonep = marpaWrapperGrammar_clonep(marpaWrapperGrammarp);
 
-  fprintf(stderr, "OK: %p\n", marpaWrapperGrammarp);
+  marpaWrapperGrammar_freev(marpaWrapperGrammarClonep);
+  marpaWrapperGrammar_freev(marpaWrapperGrammarp);
 
   return 0;
 }
