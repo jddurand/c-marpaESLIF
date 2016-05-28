5dc75e836f4238c2618554706dd3e5da1a4f860e
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-05-28 10:40:14 +0200

    dev+

--- test/marpaWrapper/grammar.c
+++ test/marpaWrapper/grammar.c
@@ -2,11 +2,26 @@
 #include <stdio.h>
 #include "marpaWrapper/grammar.h"
 
-int main(int argc, char **argv) {
-  marpaWrapperGrammar_t *marpaWrapperGrammarp;
-  marpaWrapperGrammar_t *marpaWrapperGrammarClonep;
+enum { S = 0, E, op, number, MAX_SYMBOL };
 
-  marpaWrapperGrammarp      = marpaWrapperGrammar_newp(NULL);
+int main(int argc, char **argv) {
+  marpaWrapperGrammar_t       *marpaWrapperGrammarp;
+  marpaWrapperGrammar_t      *marpaWrapperGrammarClonep;
+  marpaWrapperGrammarSymbol_t *symbolp[MAX_SYMBOL];
+  int                         i;
+
+  marpaWrapperGrammarp = marpaWrapperGrammar_newp(NULL);
+  
+  for (i = 0; i < MAX_SYMBOL; i++) {
+    marpaWrapperGrammarSymbolOption_t option;
+
+    option.datavp = NULL;
+    option.terminalb = 0;
+    option.startb = (i == 0) ? 1 : 0;
+    option.eventSeti = MARPAWRAPPERGRAMMAR_EVENTTYPE_NONE;
+    symbolp[i] = marpaWrapperGrammar_newSymbolp(marpaWrapperGrammarp, &option);
+  }
+
   marpaWrapperGrammarClonep = marpaWrapperGrammar_clonep(marpaWrapperGrammarp);
 
   marpaWrapperGrammar_freev(marpaWrapperGrammarClonep);
