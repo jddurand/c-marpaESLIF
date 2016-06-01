785371feae798957134e8c00ae845e941c530e5c
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-06-01 07:24:24 +0200

    dev+ [ci skip]

--- include/marpaWrapper/progress.h
+++ include/marpaWrapper/progress.h
@@ -1,11 +1,21 @@
 #ifndef MARPAWRAPPER_PROGRESS
 #define MARPAWRAPPER_PROGRESS
 
+#include "marpaWrapper/export.h"
+
 typedef struct marpaWrapperProgress {
   int earleySetIdi;
   int earleySetOriginIdi;
   int rulei;
   int positioni;
 } marpaWrapperProgress_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  marpaWrapper_EXPORT short marpaWrapperProgressb(marpaWrapperRecognizer_t *marpaWrapperRecognizerp, int starti, int endi, size_t *nmarpaWrapperProgressip, marpaWrapperProgress_t ***marpaWrapperProgressppp);
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* MARPAWRAPPER_PROGRESS */
