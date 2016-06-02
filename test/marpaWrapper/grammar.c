bf501f14799c6e86f22c023651a52771a13a71e6
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-06-02 17:47:18 +0200

    dev+ marpaWrapperRecognizer_completeb [ci skip]

--- test/marpaWrapper/grammar.c
+++ test/marpaWrapper/grammar.c
@@ -44,6 +44,7 @@ int main(int argc, char **argv) {
   
   marpaWrapperRecognizerp = marpaWrapperRecognizer_newp(marpaWrapperGrammarp, &marpaWrapperRecognizerOption);
   marpaWrapperRecognizer_alternativeb(marpaWrapperRecognizerp, 15, 0, 0);
+  marpaWrapperRecognizer_completeb(marpaWrapperRecognizerp);
   marpaWrapperRecognizer_freev(marpaWrapperRecognizerp);
   GENERICLOGGER_FREE(marpaWrapperRecognizerOption.genericLoggerp);
 
