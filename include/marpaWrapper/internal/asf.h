e1ffae507143d590d7fe40fae216def47bd2cb61
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-06-26 12:50:26 +0200

    ASF dev+

--- include/marpaWrapper/internal/asf.h
+++ include/marpaWrapper/internal/asf.h
@@ -7,35 +7,61 @@
 #include "marpaWrapper/asf.h"
 #include "marpa.h"
 
-typedef struct marpaWrapperAsfNidset {
-  int    id;
-  size_t nIdl;
+typedef struct marpaWrapperAsfIdset {
+  int    idi;
+  size_t countl;
   int   *idip;
-} marpaWrapperAsfNidset_t;
+} marpaWrapperAsfIdset_t;
+
+typedef marpaWrapperAsfIdset_t marpaWrapperAsfNidset_t;
+typedef marpaWrapperAsfIdset_t marpaWrapperAsfPowerset_t;
+
+typedef enum marpaWrapperAsfIdsete {
+  MARPAWRAPPERASFIDSET_NIDSET = 0,
+  MARPAWRAPPERASFIDSET_POWERSET,
+  _MARPAWRAPPERASFIDSET_IDSETE_MAX
+} marpaWrapperAsfIdsete_t;
+
+typedef struct marpaWrapperAsfGlade {
+  int   idi;
+  size_t nSymboll;
+  int   *symbolip;
+  short registeredb;
+} marpaWrapperAsfGlade_t;
+
+typedef int *andNodeStack_t;
+
+typedef struct marpaWrapperAsfOrNode {
+  size_t  nAndNodel;
+  int    *andNodep;
+} marpaWrapperAsfOrNode_t;
+
+typedef struct marpaWrapperAsfNook {
+  int orNodeIdi;
+  int firstChoicei;
+  int lastChoicei;
+  int parentOrNodeIdi;
+} marpaWrapperAsfNook_t;
 
 struct marpaWrapperAsf {
-  genericLogger_t          *genericLoggerp;
-  marpaWrapperRecognizer_t *marpaWrapperRecognizerp;
-  marpaWrapperAsfOption_t   marpaWrapperAsfOption;
-  Marpa_Bocage              marpaBocagep;
-  Marpa_Order               marpaOrderp;
+  genericLogger_t           *genericLoggerp;
+  marpaWrapperRecognizer_t  *marpaWrapperRecognizerp;
+  marpaWrapperAsfOption_t    marpaWrapperAsfOption;
+  Marpa_Bocage               marpaBocagep;
+  Marpa_Order                marpaOrderp;
 
-  /* Temporary Storage of AND Node Ids */
-  size_t                        sizeAndNodeIdl;     /* Allocated size */
-  size_t                        nAndNodeIdl;        /* Used size      */
-  int                          *andNodeIdip;
+  /* Memoization */
+  genericStack_t            *orNodeStackp;
+  genericHash_t             *intsetHashp;
+  genericStack_t            *nidsetStackp;
+  genericStack_t            *powersetStackp;
+  genericStack_t            *nookStackp;
+  marpaWrapperAsfNidset_t   *lastNidsetp;
+  marpaWrapperAsfPowerset_t *lastPowersetp;
+  genericStack_t            *gladeStackp;
 
-  /* Storage of OR Node Ids */
-  size_t                        sizeOrNodeAndNodeIdl;     /* Allocated size */
-  size_t                        nOrNodeAndNodeIdl;        /* Used size      */
-  int                         **orNodeAndNodeIdipp;
-
-  /* Memoization of visited glades */
-  genericHash_t                *intsetHashp;
-  genericStack_t               *nidsetStackp;
-
+  /* Memoization of choices */
   int                           nextIntseti;
-
 };
 
 #endif /* MARPAWRAPPER_INTERNAL_ASF_H */
