cefcd9f027b899cdbc361521aae0abb26f1aa928
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-07-10 09:06:55 +0200

    ASF dev+

--- include/marpaWrapper/asf.h
+++ include/marpaWrapper/asf.h
@@ -31,7 +31,8 @@ extern "C" {
   marpaWrapper_EXPORT marpaWrapperAsf_t     *marpaWrapperAsf_newp(marpaWrapperRecognizer_t *marpaWrapperRecognizerp, marpaWrapperAsfOption_t *marpaWrapperAsfOptionp);
   marpaWrapper_EXPORT void                  *marpaWrapperAsf_traversep(marpaWrapperAsf_t *marpaWrapperAsfp, traverserCallback_t traverserCallbackp, void *userDatavp);
   marpaWrapper_EXPORT short                  marpaWrapperAsf_rh_lengthb(marpaWrapperAsfTraverser_t *traverserp, size_t *lengthlp);
-  marpaWrapper_EXPORT genericStack_t        *marpaWrapperAsf_rh_valuesp(marpaWrapperAsfTraverser_t *traverserp);
+  marpaWrapper_EXPORT genericStack_t        *marpaWrapperAsf_rh_valuesStackp(marpaWrapperAsfTraverser_t *traverserp);
+  marpaWrapper_EXPORT genericStack_t        *marpaWrapperAsf_rh_allChoicesStackp(marpaWrapperAsfTraverser_t *traverserp);
   marpaWrapper_EXPORT void                  *marpaWrapperAsf_rh_valuep(marpaWrapperAsfTraverser_t *traverserp, size_t rhIxi);
   marpaWrapper_EXPORT genericStack_t        *marpaWrapperAsf_allchoicesp(marpaWrapperAsf_t *marpaWrapperAsfp, marpaWrapperAsfGlade_t *gladep);
   marpaWrapper_EXPORT marpaWrapperGrammar_t *marpaWrapperAsf_grammarp(marpaWrapperAsf_t *marpaWrapperAsfp);
