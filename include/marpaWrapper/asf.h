f1f2e41d702cb0b0486993ba635c53f9d5aa7722
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-07-11 17:47:18 +0200

    marpaWrapperAsf_nextSymchb

--- include/marpaWrapper/asf.h
+++ include/marpaWrapper/asf.h
@@ -34,6 +34,7 @@ extern "C" {
   marpaWrapper_EXPORT short                  marpaWrapperAsf_rh_symbolIdb(marpaWrapperAsfTraverser_t *traverserp, int *symbolIdip);
   marpaWrapper_EXPORT short                  marpaWrapperAsf_rh_ruleIdb(marpaWrapperAsfTraverser_t *traverserp, int *ruleIdip);
   marpaWrapper_EXPORT short                  marpaWrapperAsf_nextFactoringb(marpaWrapperAsfTraverser_t *traverserp, int *factoringIxip);
+  marpaWrapper_EXPORT short                  marpaWrapperAsf_nextSymchb(marpaWrapperAsfTraverser_t *traverserp, int *symchIxip);
   marpaWrapper_EXPORT genericStack_t        *marpaWrapperAsf_rh_valuesStackp(marpaWrapperAsfTraverser_t *traverserp);
   marpaWrapper_EXPORT genericStack_t        *marpaWrapperAsf_rh_allChoicesStackp(marpaWrapperAsfTraverser_t *traverserp);
   marpaWrapper_EXPORT void                  *marpaWrapperAsf_rh_valuep(marpaWrapperAsfTraverser_t *traverserp, size_t rhIxi);
