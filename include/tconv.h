a9fc9746b87b87170345d3e48eb9fe65fc38a668
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-09-18 09:23:33 +0200

    Fix includes and their install, version 1.0.1

--- include/tconv.h
+++ include/tconv.h
@@ -2,8 +2,8 @@
 #define TCONV_H
 
 #include <stddef.h>
-#include "tconv/export.h"
-#include "tconv/ext.h"
+#include <tconv/export.h>
+#include <tconv/ext.h>
 
 TCONV_EXPORT tconv_t  tconv_open(const char *tocodes, const char *fromcodes);
 TCONV_EXPORT size_t   tconv(tconv_t tconvp, char **inbufpp, size_t *inbytesleftlp, char **outbufpp, size_t *outbytesleftlp);
