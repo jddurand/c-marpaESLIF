abe1fb4644f6f6a35b36186da2d15ec0a129207f
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-09-18 09:23:33 +0200

    Fix includes and their install, version 1.0.1

--- include/tconv/charset/ICU.h
+++ include/tconv/charset/ICU.h
@@ -3,14 +3,11 @@
 
 #include <stddef.h>
 
-#include "tconv/export.h"
-
 typedef struct tconv_charset_ICU_option {
   int confidencei;                    /* Default: 10 */
 } tconv_charset_ICU_option_t;
 
-#include "tconv.h"
-#include "tconv/export.h"
+#include <tconv.h>
 
 TCONV_NO_EXPORT void *tconv_charset_ICU_new (tconv_t tconv, void *optionp);
 TCONV_NO_EXPORT char *tconv_charset_ICU_run (tconv_t tconv, void *contextp, char *bytep, size_t bytel);
