8bcbaa64daa1216a28a703eac8ecc62ac6ddfcb4
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-09-20 05:01:34 +0200

    Fix include, version 1.0.10

--- include/marpaWrapper.h
+++ include/marpaWrapper.h
@@ -1,9 +1,9 @@
 #ifndef MARPAWRAPPER_H
 #define MARPAWRAPPER_H
 
-#include "marpaWrapper/grammar.h"
-#include "marpaWrapper/recognizer.h"
-#include "marpaWrapper/value.h"
-#include "marpaWrapper/asf.h"
+#include <marpaWrapper/grammar.h>
+#include <marpaWrapper/recognizer.h>
+#include <marpaWrapper/value.h>
+#include <marpaWrapper/asf.h>
 
 #endif /* MARPAWRAPPER_H */
