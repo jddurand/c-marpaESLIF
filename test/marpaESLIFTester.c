2ac272a706eab087a29eb332243b7536f479aa37
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-10-02 15:17:47 +0200

    dev+

--- test/marpaESLIFTester.c
+++ test/marpaESLIFTester.c
@@ -1,1 +1,20 @@
-int main() { return 0; }
+#include <stdlib.h>
+#include <genericLogger.h>
+#include <marpaESLIF.h>
+
+int main() {
+  marpaESLIFOption_t marpaESLIFOption;
+  marpaESLIF_t      *marpaESLIFp;
+  int                exiti = 0;
+
+  marpaESLIFOption.genericLoggerp = GENERICLOGGER_NEW(GENERICLOGGER_LOGLEVEL_TRACE);
+  marpaESLIFp = marpaESLIF_newp(&marpaESLIFOption);
+  if (marpaESLIFp == NULL) {
+    exiti = 1;
+  }
+
+  marpaESLIF_freev(marpaESLIFp);
+  GENERICLOGGER_FREE(marpaESLIFOption.genericLoggerp);
+
+  return exiti;
+}
