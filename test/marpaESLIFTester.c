1557a787c679ab5e5891b13f486399df9433b69b
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-11-17 06:23:47 +0100

    action dev+

--- test/marpaESLIFTester.c
+++ test/marpaESLIFTester.c
@@ -14,7 +14,7 @@ typedef struct marpaESLIFTester_context {
 } marpaESLIFTester_context_t;
 
 const static char *metags = "\n"
-"<null statement> ::= ';' action => 'myaction' assoc => left\n"
+"<null statement> ::= ';' action => 'myaction' assoc => left separator => X\n"
 ;
 
 int main() {
