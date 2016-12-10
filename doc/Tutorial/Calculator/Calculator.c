0936063f65ee9b5d77bfd2ab7ff4bdfcb53c48c3
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-12-10 17:44:16 +0100

    Calculator [ci skip]

--- doc/Tutorial/Calculator/Calculator.c
+++ doc/Tutorial/Calculator/Calculator.c
@@ -4,6 +4,13 @@
 #include <marpaESLIF.h>
 #include <genericLogger.h>
 
+typedef struct reader_context {
+  char            *inputs;
+  size_t           inputl;
+} reader_context_t;
+
+static short inputReaderb(void *userDatavp, char **inputsp, size_t *inputlp, short *eofbp, short *characterStreambp, char **encodingOfEncodingsp, char **encodingsp, size_t *encodinglp);
+
 const static char *grammars =
   ":default ::= action => do_double\n"
   ":discard ::= [\\s]\n"
@@ -23,6 +30,10 @@ int main() {
   marpaESLIF_t                *marpaESLIFp;
   marpaESLIFGrammarOption_t    marpaESLIFGrammarOption;
   marpaESLIFGrammar_t         *marpaESLIFGrammarp;
+  reader_context_t             reader_context;
+  marpaESLIFRecognizerOption_t marpaESLIFRecognizerOption;
+  marpaESLIFRecognizer_t      *marpaESLIFRecognizerp;
+  char                         inputs[4096];
   int                          exiti;
 
   genericLoggerp = GENERICLOGGER_NEW(GENERICLOGGER_LOGLEVEL_DEBUG);
@@ -47,9 +58,53 @@ int main() {
     exit(1);
   }
 
+  marpaESLIFRecognizerOption.userDatavp                = &reader_context;
+  marpaESLIFRecognizerOption.marpaESLIFReaderCallbackp = inputReaderb;
+  marpaESLIFRecognizerOption.disableThresholdb         = 0;  /* No disable of threshold warning */
+  marpaESLIFRecognizerOption.exhaustedb                = 0;  /* No exhaustion event */
+  marpaESLIFRecognizerOption.newlineb                  = 1;  /* Do newline counting */
+  marpaESLIFRecognizerOption.bufsizl                   = 0;  /* Recommended value */
+  marpaESLIFRecognizerOption.buftriggerperci           = 50; /* Recommended value */
+  marpaESLIFRecognizerOption.bufaddperci               = 50; /* Recommended value */
+  marpaESLIFRecognizerp = marpaESLIFRecognizer_newp(marpaESLIFGrammarp, &marpaESLIFRecognizerOption);
+  if (marpaESLIFRecognizerp == NULL) {
+    exit(1);
+  }
+
+  if (fgets(inputs, sizeof(inputs), stdin) == NULL) {
+    perror("fgets");
+    exit(1);
+  }
+  reader_context.inputs = inputs;
+  reader_context.inputl = strlen(inputs);
+  if (! marpaESLIFRecognizer_scanb(marpaESLIFRecognizerp,
+                                   0 /* initialEventsb */,
+                                   NULL /* continuebp*/,
+                                   NULL /* exhaustedbp */)) {
+    exit(1);
+  }
+
+  marpaESLIFRecognizer_freev(marpaESLIFRecognizerp);
   marpaESLIFGrammar_freev(marpaESLIFGrammarp);
   marpaESLIF_freev(marpaESLIFp);
   GENERICLOGGER_FREE(genericLoggerp);
 
   exit(0);
+}
+
+/*****************************************************************************/
+static short inputReaderb(void *userDatavp, char **inputsp, size_t *inputlp, short *eofbp, short *characterStreambp, char **encodingOfEncodingsp, char **encodingsp, size_t *encodinglp)
+/*****************************************************************************/
+{
+  reader_context_t *reader_contextp = (reader_context_t *) userDatavp;
+  
+  *inputsp              = reader_contextp->inputs;
+  *inputlp              = reader_contextp->inputl;
+  *eofbp                = 1;    /* One chunk */
+  *characterStreambp    = 1;    /* We say this is a stream of characters */
+  *encodingOfEncodingsp = NULL; /* let marpaESLIF deal with encoding */
+  *encodingsp           = NULL;
+  *encodinglp           = 0;
+
+  return 1;
 }
