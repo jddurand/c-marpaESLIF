c4085c384b580575661e3575fae522623074b0b6
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-12-10 21:40:51 +0100

    Calculator tutorial, version 1.0.1

--- doc/Tutorial/Calculator/Calculator.c
+++ doc/Tutorial/Calculator/Calculator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <marpaESLIF.h>
 #include <genericLogger.h>
 
@@ -10,20 +11,21 @@ typedef struct reader_context {
 } reader_context_t;
 
 typedef enum stack_context {
-  INTEGER_CONTEXT = 1
+  INTEGER_CONTEXT = 1,
+  DOUBLE_CONTEXT = 1
 } stack_context_t;
 
-static short inputReaderb(void *userDatavp, char **inputsp, size_t *inputlp, short *eofbp, short *characterStreambp, char **encodingOfEncodingsp, char **encodingsp, size_t *encodinglp);
-static short do_int(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int arg0i, int argni, int resulti, short nullableb);
-static short do_paren(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int arg0i, int argni, int resulti, short nullableb);
-static short do_double(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int arg0i, int argni, int resulti, short nullableb);
+static short                         inputReaderb(void *userDatavp, char **inputsp, size_t *inputlp, short *eofbp, short *characterStreambp, char **encodingOfEncodingsp, char **encodingsp, size_t *encodinglp);
+static marpaESLIFValueRuleCallback_t ruleActionResolverp(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, char *actions);
+static short                         do_int(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int arg0i, int argni, int resulti, short nullableb);
+static short                         do_double(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int arg0i, int argni, int resulti, short nullableb);
 
 const static char *grammars =
   ":default ::= action => do_double\n"
   ":discard ::= [\\s]\n"
   "Expression ::=\n"
   "    /[\\d]+/                          action => do_int\n"
-  "    | '(' Expression ')'              assoc => group action => do_paren\n"
+  "    | '(' Expression ')'              assoc => group action => ::copy[1]\n"
   "   ||     Expression '**' Expression  assoc => right\n"
   "   ||     Expression  '*' Expression\n"
   "    |     Expression  '/' Expression\n"
@@ -37,14 +39,21 @@ int main() {
   marpaESLIF_t                *marpaESLIFp;
   marpaESLIFGrammarOption_t    marpaESLIFGrammarOption;
   marpaESLIFGrammar_t         *marpaESLIFGrammarp;
+  int                          leveli;
+  int                          ngrammari;
+  char                        *grammarshows;
   reader_context_t             reader_context;
   marpaESLIFRecognizerOption_t marpaESLIFRecognizerOption;
   marpaESLIFRecognizer_t      *marpaESLIFRecognizerp;
   char                         inputs[4096];
   marpaESLIFValueOption_t      marpaESLIFValueOption;
   marpaESLIFValue_t           *marpaESLIFValuep;
+  marpaESLIFValueResult_t      marpaESLIFValueResult;
   int                          rcValueb;
-
+  char                        *offsetp;
+  size_t                       lengthl;
+  char                        *expressions;
+  
   genericLoggerp = GENERICLOGGER_NEW(GENERICLOGGER_LOGLEVEL_DEBUG);
   if (genericLoggerp == NULL) {
     perror("GENERICLOGGER_NEW");
@@ -66,6 +75,19 @@ int main() {
   if (marpaESLIFGrammarp == NULL) {
     exit(1);
   }
+
+  /* Dump grammar */
+  /*
+  if (marpaESLIFGrammar_ngrammarib(marpaESLIFGrammarp, &ngrammari)) {
+    for (leveli = 0; leveli < ngrammari; leveli++) {
+      if (marpaESLIFGrammar_grammarshowform_by_levelb(marpaESLIFGrammarp, &grammarshows, leveli, NULL)) {
+        GENERICLOGGER_INFO (marpaESLIFOption.genericLoggerp, "-------------------------");
+        GENERICLOGGER_INFOF(marpaESLIFOption.genericLoggerp, "TEST grammar at level %d:", leveli);
+        GENERICLOGGER_INFOF(marpaESLIFOption.genericLoggerp, "-------------------------\n\n%s", grammarshows);
+      }
+    }
+  }
+  */
 
   marpaESLIFRecognizerOption.userDatavp                = &reader_context;
   marpaESLIFRecognizerOption.marpaESLIFReaderCallbackp = inputReaderb;
@@ -93,10 +115,8 @@ int main() {
     exit(1);
   }
 
-  /* marpaESLIFRecognizer_progressLogb(marpaESLIFRecognizerp, -1, -1, GENERICLOGGER_LOGLEVEL_DEBUG); */
-
   marpaESLIFValueOption.userDatavp             = NULL; /* No context... why not */
-  marpaESLIFValueOption.ruleActionResolverp    = NULL; /* No rule action resolver! */
+  marpaESLIFValueOption.ruleActionResolverp    = ruleActionResolverp;
   marpaESLIFValueOption.symbolActionResolverp  = NULL; /* No symbol action resolver... Okay we use the default */
   marpaESLIFValueOption.freeActionResolverp    = NULL; /* No free action resolver... Okay if we generate no pointer */
   marpaESLIFValueOption.highRankOnlyb          = 1;    /* Recommended value */
@@ -108,14 +128,30 @@ int main() {
   if (marpaESLIFValuep == NULL) {
     exit(1);
   }
-  while (1) {
-    rcValueb = marpaESLIFValue_valueb(marpaESLIFValuep, NULL);
-    if (rcValueb < 0) {
-      exit(1);
-    } else if (rcValueb == 0) {
-      break;
-    }
+  if (! marpaESLIFValue_valueb(marpaESLIFValuep, &marpaESLIFValueResult)) {
+    exit(1);
   }
+  if (! marpaESLIFRecognizer_last_completedb(marpaESLIFRecognizerp, "Expression", &offsetp, &lengthl)) {
+    fprintf(stderr, "marpaESLIFRecognizer_last_completedb failure!\n");
+    exit(1);
+  }
+
+  expressions = (char *) malloc(lengthl + 1);
+  memcpy(expressions, inputs + (size_t) offsetp, lengthl);
+  expressions[lengthl] = '\0';
+
+  switch (marpaESLIFValueResult.type) {
+  case MARPAESLIF_STACK_TYPE_INT:
+    fprintf(stdout, "%s = %d\n", expressions, marpaESLIFValueResult.u.i);
+    break;
+  case MARPAESLIF_STACK_TYPE_DOUBLE:
+    fprintf(stdout, "%s = %f\n", expressions, marpaESLIFValueResult.u.d);
+    break;
+  default:
+    fprintf(stdout, "Result is of unexpected type %d!\n", marpaESLIFValueResult.type);
+    exit(1);
+  }
+  free(expressions);
 
   marpaESLIFRecognizer_freev(marpaESLIFRecognizerp);
   marpaESLIFGrammar_freev(marpaESLIFGrammarp);
@@ -147,7 +183,6 @@ static short do_int(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int a
 /*****************************************************************************/
 {
   /* /[\d]+/  action => do_int */
-
   void   *bytep;
   size_t  bytel;
   int     i;
@@ -165,6 +200,7 @@ static short do_int(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int a
   i = atoi((char *) bytep);
 
   if (! marpaESLIFValue_stack_set_intb(marpaESLIFValuep, resulti, INTEGER_CONTEXT /* Anything but 0 */, i)) {
+    return 0;
   }
 
   return 1; /* Ok */
@@ -174,10 +210,165 @@ static short do_int(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int a
 static short do_double(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int arg0i, int argni, int resulti, short nullableb)
 /*****************************************************************************/
 {
+  /* Expression OPERATOR Expression */
+  int     i[2];
+  double  d[2];
+  short   intb[2];
+  short   doubleb[2];
+  void   *bytep;
+  size_t  bytel;
+  int     valuei;
+  double  valued;
+  short   resultIsIntb;
+
+  if (nullableb) {
+    fprintf(stderr, "Nullable is impossible here!\n");
+    return 0;
+  }
+
+  /* First expression is an int or a double */
+  if (! marpaESLIFValue_stack_is_intb(marpaESLIFValuep, arg0i, &(intb[0]))) {
+    fprintf(stderr, "marpaESLIFValue_stack_is_intb failure!\n");
+    return 0;
+  }
+  if (! intb[0]) {
+    /* Then it must be a double */
+    if (! marpaESLIFValue_stack_is_doubleb(marpaESLIFValuep, arg0i, &(doubleb[0]))) {
+      fprintf(stderr, "marpaESLIFValue_stack_is_doubleb failure!\n");
+      return 0;
+    }
+    if (! doubleb[0]) {
+      fprintf(stderr, "First expression is neither an int or a double!\n");
+      return 0;
+    }
+  }
+
+  /* Idem for second expression */
+  if (! marpaESLIFValue_stack_is_intb(marpaESLIFValuep, argni, &(intb[1]))) {
+    fprintf(stderr, "marpaESLIFValue_stack_is_intb failure!\n");
+    return 0;
+  }
+  if (! intb[1]) {
+    /* Then it must be a double */
+    if (! marpaESLIFValue_stack_is_doubleb(marpaESLIFValuep, argni, &(doubleb[1]))) {
+      fprintf(stderr, "marpaESLIFValue_stack_is_doubleb failure!\n");
+      return 0;
+    }
+    if (! doubleb[1]) {
+      fprintf(stderr, "Second expression is neither an int or a double!\n");
+      return 0;
+    }
+  }
+
+  /* Get values */
+  if (intb[0]) {
+    if (! marpaESLIFValue_stack_get_intb(marpaESLIFValuep, arg0i, NULL /* contextip */, &(i[0]))) {
+      fprintf(stderr, "marpaESLIFValue_stack_get_intb failure!\n");
+      return 0;
+    }
+  } else {
+    if (! marpaESLIFValue_stack_get_doubleb(marpaESLIFValuep, arg0i, NULL /* contextip */, &(d[0]))) {
+      fprintf(stderr, "marpaESLIFValue_stack_get_double failure!\n");
+      return 0;
+    }
+  }
+  if (intb[1]) {
+    if (! marpaESLIFValue_stack_get_intb(marpaESLIFValuep, argni, NULL /* contextip */, &(i[1]))) {
+      fprintf(stderr, "marpaESLIFValue_stack_get_intb failure!\n");
+      return 0;
+    }
+  } else {
+    if (! marpaESLIFValue_stack_get_doubleb(marpaESLIFValuep, argni, NULL /* contextip */, &(d[1]))) {
+      fprintf(stderr, "marpaESLIFValue_stack_get_double failure!\n");
+      return 0;
+    }
+  }
+  
+  /* Operator is a lexeme */
+  if (! marpaESLIFValue_stack_get_arrayb(marpaESLIFValuep, arg0i + 1, NULL /* contextip */, &bytep, &bytel, NULL /* shallowbp */)) {
+    fprintf(stderr, "marpaESLIFValue_stack_get_arrayb failure!\n");
+    return 0;
+  }
+  /* For convenience, marpaESLIF is ALWAYS putting a NUL character after every lexeme buffer. */
+  if (strcmp((char *) bytep, "**") == 0) {
+    /* Both are int ? */
+    if (intb[0] && intb[1]) {
+      /* fprintf(stdout, "%d ** %d\n", i[0], i[1]); */
+      valued = pow((double) i[0], (double) i[1]);
+    } else {
+      /* fprintf(stdout, "%f ** %f\n", (intb[0] ? (double) i[0] : d[0]), (intb[1] ? (double) i[1] : d[1])); */
+      valued = pow((intb[0] ? (double) i[0] : d[0]), (intb[1] ? (double) i[1] : d[1]));
+    }
+    resultIsIntb = 0;
+  } else if (strcmp((char *) bytep, "*") == 0) {
+    /* Both are int ? */
+    if (intb[0] && intb[1]) {
+      /* fprintf(stdout, "%d * %d\n", i[0], i[1]); */
+      valuei = i[0] * i[1];
+      resultIsIntb = 1;
+    } else {
+      /* fprintf(stdout, "%f * %f\n", (intb[0] ? (double) i[0] : d[0]), (intb[1] ? (double) i[1] : d[1])); */
+      valued = (intb[0] ? (double) i[0] : d[0]) * (intb[1] ? (double) i[1] : d[1]);
+      resultIsIntb = 0;
+    }
+  } else if (strcmp((char *) bytep, "/") == 0) {
+    /* Both are int ? */
+    if (intb[0] && intb[1]) {
+      if (intb[1] == 0) {
+        fprintf(stderr, "Division by zero!\n");
+        return 0;
+      }
+      /* fprintf(stdout, "%f / %f\n", (double) i[0], (double) i[1]); */
+      valued = (double) i[0] / (double) i[1];
+    } else {
+      if ((intb[1] ? (double) i[1] : d[1]) < 0.E-8) {
+        fprintf(stderr, "Division by zero (well, value is < 10E-8) !\n");
+        return 0;
+      }
+      /* fprintf(stdout, "%f / %f\n", (intb[0] ? (double) i[0] : d[0]), (intb[1] ? (double) i[1] : d[1])); */
+      valued = (intb[0] ? (double) i[0] : d[0]) / (intb[1] ? (double) i[1] : d[1]);
+    }
+    resultIsIntb = 0;
+  } else if (strcmp((char *) bytep, "+") == 0) {
+    /* Both are int ? */
+    if (intb[0] && intb[1]) {
+      /* fprintf(stdout, "%d + %d\n", i[0], i[1]); */
+      valuei = i[0] + i[1];
+      resultIsIntb = 1;
+    } else {
+      /* fprintf(stdout, "%f + %f\n", (intb[0] ? (double) i[0] : d[0]), (intb[1] ? (double) i[1] : d[1])); */
+      valued = (intb[0] ? (double) i[0] : d[0]) + (intb[1] ? (double) i[1] : d[1]);
+      resultIsIntb = 0;
+    }
+  } else if (strcmp((char *) bytep, "-") == 0) {
+    /* Both are int ? */
+    if (intb[0] && intb[1]) {
+      /* fprintf(stdout, "%d - %d\n", i[0], i[1]); */
+      valuei = i[0] - i[1];
+      resultIsIntb = 1;
+    } else {
+      /* fprintf(stdout, "%f - %f\n", (intb[0] ? (double) i[0] : d[0]), (intb[1] ? (double) i[1] : d[1])); */
+      valued = (intb[0] ? (double) i[0] : d[0]) - (intb[1] ? (double) i[1] : d[1]);
+      resultIsIntb = 0;
+    }
+  } else {
+    fprintf(stderr, "Unknown operator %s!\n", (char *) bytep);
+    return 0;
+  }
+
+  return resultIsIntb ? marpaESLIFValue_stack_set_intb(marpaESLIFValuep, resulti, INTEGER_CONTEXT, valuei) : marpaESLIFValue_stack_set_doubleb(marpaESLIFValuep, resulti, DOUBLE_CONTEXT, valued);
 }
 
 /*****************************************************************************/
-static short do_paren(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, int arg0i, int argni, int resulti, short nullableb)
+static marpaESLIFValueRuleCallback_t ruleActionResolverp(void *userDatavp, marpaESLIFValue_t *marpaESLIFValuep, char *actions)
 /*****************************************************************************/
 {
+  if (strcmp(actions, "do_int") == 0) {
+    return do_int;
+  } else if (strcmp(actions, "do_double") == 0) {
+    return do_double;
+  } else {
+    fprintf(stderr, "Unknown action %s!\n", actions);
+    return NULL;
+  }
 }
