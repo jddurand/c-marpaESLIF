d61699fd13913ed319d6309fa4e376a2dd3b985d
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-12-13 14:30:27 +0100

    jni

--- src/bindings/java/marpaESLIF.c
+++ src/bindings/java/marpaESLIF.c
@@ -1,4 +1,4 @@
-/* Java JNI bindings for marpaESLIF */
+/* Java JNI bindings for marpaESLIF                          */
 #include <jni.h>
 #include <marpaESLIF.h>
 #include <stdlib.h>
@@ -6,68 +6,106 @@
 #include <string.h>
 #include <genericLogger.h>
 
+/* It is safe to store this value via JNI_OnLoad */
 static JavaVM *g_vmp;
 
-#define JAVA_UTIL_LOGGING_LOGGER_STRING                  "java/util/logging/Logger"
-#define SEVERE_STRING                                    "severe"
-#define JAVA_UTIL_LOGGING_LOGGER_SEVERE_SIGNATURE_STRING "(Ljava/lang/String;)V"
-
-typedef struct genericLoggerContext       genericLoggerContext_t;
-typedef enum   genericLoggerCallbackLevel genericLoggerCallbackLevel_t;
-
-enum genericLoggerCallbackLevel {
-  GENERICLOGGERCONTEXT_SEVERE,
-  GENERICLOGGERCONTEXT_WARNING,
-  GENERICLOGGERCONTEXT_INFO,
-  GENERICLOGGERCONTEXT_FINE,
-  GENERICLOGGERCONTEXT_FINER,
-  GENERICLOGGERCONTEXT_FINEST,
-  _GENERICLOGGERCONTEXT_MAX,
-};
-
-struct genericLoggerContext {
-  genericLoggerContext_t *selfp;
-  jmethodID               logMethodID[_GENERICLOGGERCONTEXT_MAX];
-}
-
+/* ---------------- */
+/* Exported methods */
+/* ---------------- */
+JNIEXPORT jint    JNICALL JNI_OnLoad(JavaVM *vmp, void* reservedp);
 JNIEXPORT jobject JNICALL Java_org_marpa_ESLIFParser_marpaESLIF_newp(JNIEnv *envp, jobject objp, jobject loggerp);
 
+/* ------------------------------ */
+/* Internal types and definitions */
+/* ------------------------------ */
+#define EXCEPTION_CLASS_STRING   "java/lang/Exception"
+#define ESLIFPARSER_CLASS_STRING "org/marpa/ESLIFParser"
+#define ESLIFPARSER_LOG_SIGNATURE "(Ljava/lang/String;)V"
+
+typedef struct marpaESLIF_genericLoggerContext marpaESLIF_genericLoggerContext_t;
+
+enum marpaESLIF_genericLoggerLevel {
+  MARPAESLIF_LOGLEVEL_TRACE = 0,
+  MARPAESLIF_LOGLEVEL_DEBUG,
+  MARPAESLIF_LOGLEVEL_INFO,
+  MARPAESLIF_LOGLEVEL_NOTICE,
+  MARPAESLIF_LOGLEVEL_WARNING,
+  MARPAESLIF_LOGLEVEL_ERROR,
+  MARPAESLIF_LOGLEVEL_CRITICAL,
+  MARPAESLIF_LOGLEVEL_ALERT,
+  MARPAESLIF_LOGLEVEL_EMERGENCY,
+  _MARPAESLIF_LOGLEVEL_MAX
+};
+
+const static char *marpaESLIF_logMethod[_MARPAESLIF_LOGLEVEL_MAX] = {
+  "trace", "debug", "info", "notice", "warning", "error", "critical", "alert", "emergency"
+};
+static jmethodID marpaESLIF_logMethodp[_MARPAESLIF_LOGLEVEL_MAX];
+
+/* -------------- */
+/* Static methods */
+/* -------------- */
 static void  genericLoggerCallback(void *userDatavp, genericLoggerLevel_t logLeveli, const char *msgs);
-static short initLogging(genericLoggerCallbackContext_t *genericLoggerCallbackContextp);
-static void  raiseException(char *msgs);
+static void  raiseException(JNIEnv *envp, char *msgs);
 
 /*****************************************************************************/
-jint JNI_OnLoad(JavaVM *vmp, void* reservedp)
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vmp, void* reservedp)
 /*****************************************************************************/
 {
+  JNIEnv   *envp = NULL;
+  jint      versioni;
+  jclass    eslifParserClassp;
+  jmethodID methodp;
+  int       i;
+
   /* It is safe to store vmp in a global variable */
   g_vmp = vmp;
 
-  return JNI_VERSION_1_1;
+  if (((*vmp)->GetEnv(vmp, (void **) &envp, JNI_VERSION_1_1) != JNI_OK) || (envp == NULL)) {
+    goto err;
+  }
+
+  eslifParserClassp = (*envp)->FindClass(envp, ESLIFPARSER_CLASS_STRING);
+  if (eslifParserClassp == NULL) {
+    raiseException(envp, "Failed to find class " ESLIFPARSER_CLASS_STRING);
+    goto err;
+  }
+  for (i = 0; i < _MARPAESLIF_LOGLEVEL_MAX; i++) {
+    methodp = (*envp)->GetMethodID(envp,
+                                   eslifParserClassp,
+                                   marpaESLIF_logMethod[i],
+                                   ESLIFPARSER_LOG_SIGNATURE);
+    if (methodp == NULL) {
+      raiseException(envp, "Failed to find logging method in " ESLIFPARSER_CLASS_STRING);
+      goto err;
+    }
+
+    marpaESLIF_logMethodp[i] = methodp;
+  }
+
+  versioni = JNI_VERSION_1_1;
+  goto done;
+
+ err:
+  versioni = -1;
+
+ done:
+  return versioni;
 }
 
 /*****************************************************************************/
-JNIEXPORT jobject JNICALL Java_org_marpa_ESLIFParser_marpaESLIF_newp(JNIEnv *envp, jobject objp, jobject loggerp)
+JNIEXPORT jobject JNICALL Java_org_marpa_ESLIFParser_marpaESLIF_newp(JNIEnv *envp, jobject objectp, jobject loggerp)
 /*****************************************************************************/
 {
   marpaESLIF_t           *marpaESLIFp           = NULL;
-  genericLoggerContext_t *genericLoggerContextp = NULL;
   genericLogger_t        *genericLoggerp        = NULL;
   jobject                 newDirectByteBufferp  = NULL;
   marpaESLIFOption_t      marpaESLIFOption;
  
   if (loggerp != NULL) {
-    genericLoggerContextp = (genericLoggerContext_t *) malloc(sizeof(genericLoggerContext_t));
-    if (genericLoggerContextp == NULL) {
-      raiseException(strerror(errno));
-      goto err;
-    }
-    if (! initLogging(genericLoggerContextp)) {
-      goto err;
-    }
-    genericLoggerp = genericLogger_newp(genericLoggerCallback, (void *) genericLoggerContextp, GENERICLOGER_LOGLEVEL_TRACE);
+    genericLoggerp = genericLogger_newp(genericLoggerCallback, (void *) objectp, GENERICLOGGER_LOGLEVEL_TRACE);
     if (genericLoggerp == NULL) {
-      raiseException(strerror(errno));
+      raiseException(envp, strerror(errno));
       goto err;
     }
   }
@@ -78,18 +116,15 @@ JNIEXPORT jobject JNICALL Java_org_marpa_ESLIFParser_marpaESLIF_newp(JNIEnv *env
     goto err;
   }
 
-  newDirectByteBufferp = NewDirectByteBuffer(envp, marpaESLIFp, sizeof(marpaESLIF_t));
+  newDirectByteBufferp = (*envp)->NewDirectByteBuffer(envp, marpaESLIFp, sizeof(marpaESLIF_t *));
   if (newDirectByteBufferp == NULL) {
-    raiseException("NewDirectByteBuffer failure");
+    raiseException(envp, "NewDirectByteBuffer failure");
     goto err;
   }
 
   goto done;
 
  err:
-  if (genericLoggerContextp == NULL) {
-    free(genericLoggerContextp);
-  }
   genericLogger_freev(&genericLoggerp);
   marpaESLIF_freev(marpaESLIFp);
 
@@ -98,19 +133,14 @@ JNIEXPORT jobject JNICALL Java_org_marpa_ESLIFParser_marpaESLIF_newp(JNIEnv *env
 }
 
 /*****************************************************************************/
-static void raiseException(char *msgs)
+static void raiseException(JNIEnv *envp, char *msgs)
 /*****************************************************************************/
 {
-  JNIEnv *envp       = NULL;
-  jclass  exceptionp = NULL;
+  jclass exceptionp = NULL;
 
-  if ((GetEnv(g_vmp, &envp, JNI_VERSION_1_1) != JNI_OK) || (envp == NULL)) {
-    return;
-  }
-
-  exceptionp = FindClass(envp, "java/lang/Exception");
+  exceptionp = (*envp)->FindClass(envp, EXCEPTION_CLASS_STRING);
   if (exceptionp != NULL) {
-    ThrowNew(envp, exceptionp, msgs);
+    (*envp)->ThrowNew(envp, exceptionp, msgs);
   }
 }
 
@@ -118,36 +148,19 @@ static void raiseException(char *msgs)
 static void genericLoggerCallback(void *userDatavp, genericLoggerLevel_t logLeveli, const char *msgs)
 /*****************************************************************************/
 {
-  genericLoggerContext_t *genericLoggerContextp = (genericLoggerContext_t *) userDatavp;
-  JNIEnv                 *envp                  = NULL;
+  jobject marpaESLIFp = (jobject) userDatavp;
+  JNIEnv *envp        = NULL;
 
-  /* The logging callack is never running in another thread - no need to attach */
-  if ((GetEnv(g_vmp, &envp, JNI_VERSION_1_1) != JNI_OK) || (envp == NULL)) {
+  /* Logging callack is never running in another thread - no need to attach */
+  if (((*g_vmp)->GetEnv(g_vmp, (void **) &envp, JNI_VERSION_1_1) != JNI_OK) || (envp == NULL)) {
     return;
   }
 
   switch (logLeveli) {
   case GENERICLOGGER_LOGLEVEL_TRACE:
-    CallVoidMethod(envp, logger, log_method_table[level], NewStringUTF(envp, msgs));
+    (*envp)->CallVoidMethod(envp, marpaESLIFp, marpaESLIF_logMethodp[MARPAESLIF_LOGLEVEL_TRACE], (*envp)->NewStringUTF(envp, msgs));
     break;
   default:
     break;
-  }
-}
-
-/*****************************************************************************/
-static void initLogging(genericLoggerCallbackContext_t *genericLoggerCallbackContextp)
-/*****************************************************************************/
-{
-  jclass loggerClassp;
-
-  loggerClassp = FindClass(envp, JAVA_UTIL_LOGGING_LOGGER_STRING);
-  if (loggerClassp == NULL) {
-    raiseException("Failed to find " JAVA_UTIL_LOGGING_LOGGER_STRING);
-  }
-
-  genericLoggerContextp->logMethodID[GENERICLOGGERCONTEXT_SEVERE] = GetMethodID(envp, loggerClassp, SEVERE_STRING, JAVA_UTIL_LOGGING_LOGGER_SEVERE_SIGNATURE_STRING);
-  if (genericLoggerContextp->logMethodID[GENERICLOGGERCONTEXT_SEVERE] == NULL) {
-    raiseException("Failed to find " JAVA_UTIL_LOGGING_LOGGER_STRING " method " JAVA_UTIL_LOGGING_LOGGER_SEVERE_SIGNATURE_STRING);
   }
 }
