0a3dcc7531b2e04c16cb1413e7bc9fbb1233d225
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-12-13 17:21:04 +0100

    jni [ci skip]

---
+++ src/java/src/org/marpa/ESLIFOption.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.marpa;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Jean-Damien Durand
+ *
+ */
+public class ESLIFOption {
+	private Logger _logger = null;
+	
+	public Logger get_logger() {
+		return _logger;
+	}
+
+	public void set_logger(Logger _logger) {
+		this._logger = _logger;
+	}
+
+}
