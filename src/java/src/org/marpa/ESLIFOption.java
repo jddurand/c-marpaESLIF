805945f86aa7d12dca287eacbba10d34ee8c19f2
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-12-14 17:54:20 +0100

    jni dev+

--- src/java/src/org/marpa/ESLIFOption.java
+++ src/java/src/org/marpa/ESLIFOption.java
@@ -3,6 +3,7 @@
  */
 package org.marpa;
 
+import java.nio.ByteBuffer;
 import java.util.logging.Logger;
 
 /**
@@ -10,14 +11,44 @@ import java.util.logging.Logger;
  *
  */
 public class ESLIFOption {
-	private Logger _logger = null;
+	private ByteBuffer _genericLoggerp        = null;
+	private ByteBuffer _genericLoggerContextp = null;
+	private Logger     _logger                = null;
 	
-	public Logger get_logger() {
+	public ESLIFOption() {
+	}
+	
+	public ESLIFOption(Logger logger) {
+		set_logger(logger);
+	}
+
+	@SuppressWarnings("unused")
+	private ByteBuffer get_genericLoggerp() {
+		return _genericLoggerp;
+	}
+
+	@SuppressWarnings("unused")
+	private void set_genericLoggerp(ByteBuffer _genericLoggerp) {
+		this._genericLoggerp = _genericLoggerp;
+	}
+
+	@SuppressWarnings("unused")
+	private ByteBuffer get_genericLoggerContextp() {
+		return _genericLoggerContextp;
+	}
+
+	@SuppressWarnings("unused")
+	private void set_genericLoggerContextp(ByteBuffer _genericLoggerContextp) {
+		this._genericLoggerContextp = _genericLoggerContextp;
+	}
+
+	@SuppressWarnings("unused")
+	private Logger get_logger() {
 		return _logger;
 	}
 
-	public void set_logger(Logger _logger) {
+	private void set_logger(Logger _logger) {
 		this._logger = _logger;
 	}
-
+	
 }
--- src/java/test/marpaESLIFApp.java
+++ src/java/test/marpaESLIFApp.java
@@ -1,12 +1,28 @@
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.marpa.ESLIF;
+import org.marpa.ESLIFOption;
 
 public class marpaESLIFApp {
+	private static final String CLASS = marpaESLIFApp.class.getName();
+	private static final Logger LOGGER = Logger.getLogger(CLASS);
 	
 	public static void main(String[] args) {
-		ESLIF eSLIF = new ESLIF();
-		eSLIF.init();
+		
+		Handler ch = new ConsoleHandler();
+		ch.setLevel(Level.ALL);
+		LOGGER.addHandler(ch);
+		LOGGER.setLevel(Level.ALL);
+		LOGGER.log(Level.CONFIG, "TEST");
+		
+		ESLIFOption eslifOption = new ESLIFOption(LOGGER);
+		ESLIF eslif = new ESLIF(eslifOption);
+		eslif.init();
         System.out.println("Hello World!"); // Display the string.
-		eSLIF.release();
+        eslif.release();
     }
 }
