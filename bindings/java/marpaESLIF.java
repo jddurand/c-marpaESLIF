0343db67fa8e25440318934d54648d6f852c5ec8
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-12-16 15:05:03 +0100

    java dev+

--- bindings/java/marpaESLIF.java
+++ bindings/java/marpaESLIF.java
@@ -1,7 +1,23 @@
 package org.parser;
 
+import org.parser.marpaESLIFWrapperJava.*;
+
 public class marpaESLIF {
+    private SWIGTYPE_p_marpaESLIF marpaESLIFp = null;
+
     static {
         System.loadLibrary("marpaESLIFWrapperJava");
+    }
+
+    marpaESLIF() {
+    	setMarpaESLIFp(org.parser.marpaESLIFWrapperJava.marpaESLIFWrapper.marpaESLIF_newp(null));
+    }
+
+    public SWIGTYPE_p_marpaESLIF getMarpaESLIFp() {
+        return marpaESLIFp;
+    }
+
+    public void setMarpaESLIFp(SWIGTYPE_p_marpaESLIF marpaESLIFp) {
+        this.marpaESLIFp = marpaESLIFp;
     }
 }
