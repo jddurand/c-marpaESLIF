320d7d0bab45f5f20cb9065a8ed1aa45eb3aa571
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-12-18 17:15:47 +0100

    JNI dev+ [ci skip]

---
+++ src/bindings/java/src/org/parser/marpa/ESLIFGrammar.java
@@ -0,0 +1,49 @@
+package org.parser.marpa;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
+public class ESLIFGrammar {
+	private ESLIF       eslif         = null;
+	private ByteBuffer  marpaGrammarp = null;
+	private native void newp(byte[] utf8);
+	private native void freev();
+	
+	/*
+	 * ********************************************
+	 * Public methods
+	 * ********************************************
+	 */
+	public ESLIFGrammar(ESLIF eslif, String grammar) throws UnsupportedEncodingException {
+		this.eslif = eslif;
+		newp(grammar.getBytes("UTF-8"));
+	}
+
+	public ESLIFLoggerInterface getLoggerInterface() {
+		ESLIFLoggerInterface loggerInterface = null;
+		if (this.eslif != null) {
+			loggerInterface = eslif.getLoggerInterface();
+		}
+		return loggerInterface;
+	}
+	
+	public void free() {
+		freev();
+	}
+
+	/*
+	 * ********************************************
+	 * Private methods - used by the JNI
+	 * ********************************************
+	 */
+	private ESLIF getEslif() {
+		return eslif;
+	}
+	private ByteBuffer getMarpaGrammarp() {
+		return marpaGrammarp;
+	}
+	private void setMarpaGrammarp(ByteBuffer marpaGrammarp) {
+		this.marpaGrammarp = marpaGrammarp;
+	}
+
+}
--- src/bindings/java/test/org/parser/marpa/ESLIFLogger.java
+++ src/bindings/java/test/org/parser/marpa/ESLIFLogger.java
@@ -6,35 +6,35 @@ public class ESLIFLogger implements ESLIFLoggerInterface {
 	}
 
 	public void debug(String message) {
-		System.out.println("<TRACE> " + message);	
+		System.out.println("<DEBUG> " + message);	
 	}
 
 	public void info(String message) {
-		System.out.println("<TRACE> " + message);	
+		System.out.println("<INFO> " + message);	
 	}
 
 	public void notice(String message) {
-		System.out.println("<TRACE> " + message);	
+		System.out.println("<NOTICE> " + message);	
 	}
 
 	public void warning(String message) {
-		System.out.println("<TRACE> " + message);	
+		System.out.println("<WARNING> " + message);	
 	}
 
 	public void error(String message) {
-		System.err.println("<TRACE> " + message);	
+		System.err.println("<ERROR> " + message);	
 	}
 
 	public void critical(String message) {
-		System.err.println("<TRACE> " + message);	
+		System.err.println("<CRITICAL> " + message);	
 	}
 
 	public void alert(String message) {
-		System.err.println("<TRACE> " + message);	
+		System.err.println("<ALERT> " + message);	
 	}
 
 	public void emergency(String message) {
-		System.err.println("<TRACE> " + message);	
+		System.err.println("<EMERGENCY> " + message);	
 	}
 }
 
