3c3d213e896c7697a05e53fb4f92d450bd05e720
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-12-19 11:49:12 +0100

    JNI dev+ [ci skip]

--- src/bindings/java/src/org/parser/marpa/ESLIFGrammar.java
+++ src/bindings/java/src/org/parser/marpa/ESLIFGrammar.java
@@ -4,11 +4,22 @@ import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 
 public class ESLIFGrammar {
-	private ESLIF       eslif         = null;
-	private ByteBuffer  marpaGrammarp = null;
-	private native void jniNew(byte[] utf8);
-	private native void jniFree();
-	private native int  jniNgrammar();
+	private ESLIF         eslif         = null;
+	private ByteBuffer    marpaGrammarp = null;
+	private native void   jniNew(byte[] utf8);
+	private native void   jniFree();
+	private native int    jniNgrammar();
+	private native int    jniCurrentLevel();
+	private native String jniCurrentDescription();
+	private native String jniDescriptionByLevel(int level);
+	private native int[]  jniCurrentRuleIds();
+	private native int[]  jniRuleIdsByLevel(int level);
+	private native String jniRuleDisplay(int rule);
+	private native String jniRuleShow(int rule);
+	private native String jniRuleDisplayByLevel(int level, int rule);
+	private native String jniRuleShowByLevel(int level, int rule);
+	private native String jniShow();
+	private native String jniShowByLevel(int level);
 	
 	/*
 	 * ********************************************
@@ -19,15 +30,45 @@ public class ESLIFGrammar {
 		this.eslif = eslif;
 		jniNew(grammar.getBytes("UTF-8"));
 	}
-
 	public void free() {
 		jniFree();
 	}
-	
 	public int ngrammar() {
 		return jniNgrammar();
 	}
-	
+	public int currentLevel() {
+		return jniCurrentLevel();
+	}
+	public String currentDescription() {
+		return jniCurrentDescription();
+	}
+	public String descriptionByLevel(int level) {
+		return jniDescriptionByLevel(level);
+	}
+	public int[] currentRuleIds() {
+		return jniCurrentRuleIds();
+	}
+	public int[] ruleIdsByLevel(int level) {
+		return jniRuleIdsByLevel(level);
+	}
+	public String ruleDisplay(int rule) {
+		return jniRuleDisplay(rule);
+	}
+	public String ruleShow(int rule) {
+		return jniRuleShow(rule);
+	}
+	public String ruleDisplayByLevel(int level, int rule) {
+		return jniRuleDisplayByLevel(level, rule);
+	}
+	public String ruleShowByLevel(int level, int rule) {
+		return jniRuleShowByLevel(level, rule);
+	}
+	public String show() {
+		return jniShow();
+	}
+	public String showByLevel(int level) {
+		return jniShowByLevel(level);
+	}
 	/*
 	 * ********************************************
 	 * Private methods - used by the JNI
