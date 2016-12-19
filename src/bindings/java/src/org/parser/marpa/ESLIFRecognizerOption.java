1e5c239ae533b80fc95b10f72150435741f5d626
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-12-19 12:24:20 +0100

    JNI dev+ [ci skip]

---
+++ src/bindings/java/src/org/parser/marpa/ESLIFRecognizerOption.java
@@ -0,0 +1,54 @@
+package org.parser.marpa;
+
+import java.io.Reader;
+
+public class ESLIFRecognizerOption {
+	private Reader  reader          = null;
+	private boolean threshold       = false;
+	private boolean newline         = false;
+	private int     bufsizl         = 0;
+	private int     buftriggerperci = 50;
+	private int     bufaddperci     = 50;
+
+	/*
+	 * ********************************************
+	 * Public methods
+	 * ********************************************
+	 */
+	public Reader getReader() {
+		return reader;
+	}
+	public void setReader(Reader reader) {
+		this.reader = reader;
+	}
+	public boolean isThreshold() {
+		return threshold;
+	}
+	public void setThreshold(boolean threshold) {
+		this.threshold = threshold;
+	}
+	public boolean isNewline() {
+		return newline;
+	}
+	public void setNewline(boolean newline) {
+		this.newline = newline;
+	}
+	public int getBufsizl() {
+		return bufsizl;
+	}
+	public void setBufsizl(int bufsizl) {
+		this.bufsizl = bufsizl;
+	}
+	public int getBuftriggerperci() {
+		return buftriggerperci;
+	}
+	public void setBuftriggerperci(int buftriggerperci) {
+		this.buftriggerperci = buftriggerperci;
+	}
+	public int getBufaddperci() {
+		return bufaddperci;
+	}
+	public void setBufaddperci(int bufaddperci) {
+		this.bufaddperci = bufaddperci;
+	} 
+}
---
+++ src/bindings/java/src/org/parser/marpa/ESLIFValueOption.java
@@ -0,0 +1,64 @@
+package org.parser.marpa;
+
+public class ESLIFValueOption {
+	private Object  object = null;
+	private boolean highRank = true;
+	private boolean orderByRank = true;
+	private boolean ambiguous = false;
+	private boolean nullParse = false;
+	private int     maxParse = 0;
+	private boolean exhausted = false;
+
+	/*
+	 * ********************************************
+	 * Public methods
+	 * ********************************************
+	 */
+	public Object getObject() {
+		return object;
+	}
+	public void setObject(Object object) {
+		this.object = object;
+	}
+	public boolean isHighRank() {
+		return highRank;
+	}
+	public void setHighRank(boolean highRank) {
+		this.highRank = highRank;
+	}
+	public boolean isOrderByRank() {
+		return orderByRank;
+	}
+	public void setOrderByRank(boolean orderByRank) {
+		this.orderByRank = orderByRank;
+	}
+	public boolean isAmbiguous() {
+		return ambiguous;
+	}
+	public void setAmbiguous(boolean ambiguous) {
+		this.ambiguous = ambiguous;
+	}
+	public boolean isNullParse() {
+		return nullParse;
+	}
+	public void setNullParse(boolean nullParse) {
+		this.nullParse = nullParse;
+	}
+	public int getMaxParse() {
+		return maxParse;
+	}
+	public void setMaxParse(int maxParse) {
+		this.maxParse = maxParse;
+	}
+	public boolean isExhausted() {
+		return exhausted;
+	}
+	/*
+	 * ********************************************
+	 * Private methods - used by the JNI
+	 * ********************************************
+	 */
+	private void setExhausted(boolean exhausted) {
+		this.exhausted = exhausted;
+	}
+}
