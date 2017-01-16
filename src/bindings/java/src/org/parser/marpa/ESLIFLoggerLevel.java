93fe921b7647ea7be681cce47663c92352d6d021
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-01-16 11:15:19 +0100

    doc [ci skip]

--- src/bindings/java/src/org/parser/marpa/ESLIFLoggerLevel.java
+++ src/bindings/java/src/org/parser/marpa/ESLIFLoggerLevel.java
@@ -4,18 +4,52 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * ESLIFLoggerLevel is a enumeration of all ESLIF logging levels.
+ * <p>
+ * This class is explicitly reproducing the formalism of the <a href="http://github.com/jddurand/c-genericLogger" target="_top">genericLogger C library</a>.
+ * 
+ * @see ESLIFLoggerInterface
+ */
 public enum ESLIFLoggerLevel {
 
 	/* Thanks to http://www.ajaxonomy.com/2007/java/making-the-most-of-java-50-enum-tricks */
 
+	/**
+	 * TRACE log level, value is {@code 0}
+	 */
 	TRACE    (0),
+	/**
+	 * DEBUG log level, value is {@code 1}
+	 */
 	DEBUG    (1),
+	/**
+	 * INFO log level, value is {@code 2}
+	 */
 	INFO     (2),
+	/**
+	 * NOTICE log level, value is {@code 3}
+	 */
 	NOTICE   (3),
+	/**
+	 * WARNING log level, value is {@code 4}
+	 */
 	WARNING  (4),
+	/**
+	 * ERROR log level, value is {@code 5}
+	 */
 	ERROR    (5),
+	/**
+	 * CRITICAL log level, value is {@code 6}
+	 */
 	CRITICAL (6),
+	/**
+	 * ALERT log level, value is {@code 7}
+	 */
 	ALERT    (7),
+	/**
+	 * EMERGENCY log level, value is {@code 8}
+	 */
 	EMERGENCY(8);
 
 	private int code;
@@ -31,6 +65,11 @@ public enum ESLIFLoggerLevel {
 		this.code = code;
 	}
 
+	/**
+	 * Get the value associated to a log level.
+	 * 
+	 * @return the log level value
+	 */
 	public int getCode() {
 		return code;
 	}
