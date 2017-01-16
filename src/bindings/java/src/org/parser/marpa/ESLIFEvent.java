4b3e2df7c30962e0cefec7f3c24e0e615674ad5d
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-01-16 16:06:27 +0100

    doc [ci skip]

--- src/bindings/java/src/org/parser/marpa/ESLIFEvent.java
+++ src/bindings/java/src/org/parser/marpa/ESLIFEvent.java
@@ -1,24 +1,48 @@
 package org.parser.marpa;
 
+/**
+ * ESLIFEvent is describing an event coming out from the grammar.
+ * 
+ * @see ESLIFEventType
+ * @see ESLIFRecognizer#events()
+ */
 public class ESLIFEvent {
 	private ESLIFEventType eslifEventType;
 	private String         symbol;
 	private String         event;
 	
+	/**
+	 * 
+	 * @param eslifEventType the event type
+	 * @param symbol the symbol name
+	 * @param event the event name
+	 */
 	ESLIFEvent(ESLIFEventType eslifEventType, String symbol, String event) {
 		this.eslifEventType = eslifEventType;
 		this.symbol         = symbol;
 		this.event          = event;
 	}
 
+	/**
+	 * 
+	 * @return the event type
+	 */
 	public ESLIFEventType getEslifEventType() {
 		return eslifEventType;
 	}
 
+	/**
+	 * 
+	 * @return the symbol name, never null
+	 */
 	public String getSymbol() {
 		return symbol;
 	}
 
+	/**
+	 * 
+	 * @return the event name, never null
+	 */
 	public String getEvent() {
 		return event;
 	}
