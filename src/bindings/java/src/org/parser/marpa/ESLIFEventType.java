a0f7a153b9b511687397d12e08877cd176d02d94
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-01-16 15:53:05 +0100

    doc [ci skip]

--- src/bindings/java/src/org/parser/marpa/ESLIFEventType.java
+++ src/bindings/java/src/org/parser/marpa/ESLIFEventType.java
@@ -4,17 +4,46 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * ESLIFEventType is an enumeration of all ESLIF possible events.
+ * 
+ * @see ESLIFEvent
+ */
 public enum ESLIFEventType {
 
 	/* Thanks to http://www.ajaxonomy.com/2007/java/making-the-most-of-java-50-enum-tricks */
 
+	/**
+	 * Should never happen -;
+	 */
 	NONE     (0x00),
+	/**
+	 * Symbol completion event
+	 */
 	COMPLETED(0x01), /* Grammar event */
+	/**
+	 * Symbol nulling event
+	 */
 	NULLED   (0x02), /* Grammar event */
+	/**
+	 * Symbol prediction event
+	 */
 	PREDICTED(0x04), /* Grammar event */
+	/**
+	 * Lexeme prediction event
+	 */
 	BEFORE   (0x08), /* Just before lexeme is commited */
+	/**
+	 * Lexeme consumption event
+	 */
 	AFTER    (0x10), /* Just after lexeme is commited */
+	/**
+	 * Exhaustion event
+	 */
 	EXHAUSTED(0x20), /* Exhaustion */
+	/**
+	 * Discard event
+	 */
 	DISCARD  (0x40); /* Discard */
 
 	private int code;
@@ -30,10 +59,20 @@ public enum ESLIFEventType {
 		this.code = code;
 	}
 
+	/**
+	 * Get the event value associated to an instance of ESLIFEventType.
+	 * 
+	 * @return the event value
+	 */
 	public int getCode() {
 		return code;
 	}
 
+	/**
+	 * Get an instance of ESLIFEventType from an event value
+	 * 
+	 * @return the ESLIFEventType instance
+	 */
 	public static ESLIFEventType get(int code) { 
 		return lookup.get(code); 
 	}
