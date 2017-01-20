8aa8148e6bfb8f37a60bb5193c076e4bab54388f
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-01-20 18:04:32 +0100

    doc [ci skip]

--- src/bindings/java/src/org/parser/marpa/ESLIFValueInterface.java
+++ src/bindings/java/src/org/parser/marpa/ESLIFValueInterface.java
@@ -1,11 +1,45 @@
 package org.parser.marpa;
 
+/**
+ * ESLIFValueInterface is giving the required methods for valuation.
+ */
+
 public interface ESLIFValueInterface {
+	/**
+	 * When the interface returns true, only the choices with the highest rank are kept.
+	 * 
+	 * @return the high-rank only preference
+	 */
 	public boolean isWithHighRankOnly();
+	/**
+	 * 
+	 * @return the rank order preference
+	 */
 	public boolean isWithOrderByRank();
+	/**
+	 * 
+	 * @return the ambiguity acceptance
+	 */
 	public boolean isWithAmbiguous();
+	/**
+	 * 
+	 * @return the null parse acceptance
+	 */
 	public boolean isWithNull();
+	/**
+	 * 
+	 * @return the maximum number of wanted parses
+	 */
 	public int     maxParses();
+	/**
+	 * This method is used by the JNI proxy to marpaESLIF library to store the final result
+	 * 
+	 * @param result the final result
+	 */
 	public void    setResult(Object result);
+	/**
+	 * 
+	 * @return the final result
+	 */
 	public Object  getResult();
 }
