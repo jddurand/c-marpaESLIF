840e5fb4fec4c0a99604361208a0202961d03e50
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-02-04 12:13:39 +0100

    Static version rework, version 1.0.2

--- include/genericLogger.h
+++ include/genericLogger.h
@@ -103,14 +103,14 @@ extern "C" {
   /*************************
    Exported symbols
   *************************/
-  GENERICLOGGER_EXPORT genericLoggerCallback_t genericLogger_defaultLogCallbackp(void);
-  GENERICLOGGER_EXPORT genericLoggerLevel_t    genericLogger_logLevel_seti(genericLogger_t *genericLoggerp, genericLoggerLevel_t logLeveli);
-  GENERICLOGGER_EXPORT genericLoggerLevel_t    genericLogger_logLevel_geti(genericLogger_t *genericLoggerp);
-  GENERICLOGGER_EXPORT genericLogger_t        *genericLogger_newp(genericLoggerCallback_t logCallbackp, void *userDatavp, genericLoggerLevel_t genericLoggerLeveli);
-  GENERICLOGGER_EXPORT genericLogger_t        *genericLogger_clonep(genericLogger_t *genericLoggerp);
-  GENERICLOGGER_EXPORT void                    genericLogger_freev(genericLogger_t **genericLoggerpp);
-  GENERICLOGGER_EXPORT void                    genericLogger_logv(genericLogger_t *genericLoggerp, genericLoggerLevel_t genericLoggerLeveli, const char *fmts, ...);
-  GENERICLOGGER_EXPORT void                    genericLogger_logapv(genericLogger_t *genericLoggerp, genericLoggerLevel_t genericLoggerLeveli, const char *fmts, va_list ap);
+  genericLogger_EXPORT genericLoggerCallback_t genericLogger_defaultLogCallbackp(void);
+  genericLogger_EXPORT genericLoggerLevel_t    genericLogger_logLevel_seti(genericLogger_t *genericLoggerp, genericLoggerLevel_t logLeveli);
+  genericLogger_EXPORT genericLoggerLevel_t    genericLogger_logLevel_geti(genericLogger_t *genericLoggerp);
+  genericLogger_EXPORT genericLogger_t        *genericLogger_newp(genericLoggerCallback_t logCallbackp, void *userDatavp, genericLoggerLevel_t genericLoggerLeveli);
+  genericLogger_EXPORT genericLogger_t        *genericLogger_clonep(genericLogger_t *genericLoggerp);
+  genericLogger_EXPORT void                    genericLogger_freev(genericLogger_t **genericLoggerpp);
+  genericLogger_EXPORT void                    genericLogger_logv(genericLogger_t *genericLoggerp, genericLoggerLevel_t genericLoggerLeveli, const char *fmts, ...);
+  genericLogger_EXPORT void                    genericLogger_logapv(genericLogger_t *genericLoggerp, genericLoggerLevel_t genericLoggerLeveli, const char *fmts, va_list ap);
 #ifdef _cpluscplus
 }
 #endif
