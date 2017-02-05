0d9079eca92423f784d1d4e1319c7ef57d161393
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-02-05 08:23:47 +0100

    Build all objects at once, version 1.0.8

--- include/tconv.h
+++ include/tconv.h
@@ -5,8 +5,8 @@
 #include <tconv/export.h>
 #include <tconv/ext.h>
 
-TCONV_EXPORT tconv_t  tconv_open(const char *tocodes, const char *fromcodes);
-TCONV_EXPORT size_t   tconv(tconv_t tconvp, char **inbufpp, size_t *inbytesleftlp, char **outbufpp, size_t *outbytesleftlp);
-TCONV_EXPORT int      tconv_close(tconv_t tconvp);
+tconv_EXPORT tconv_t  tconv_open(const char *tocodes, const char *fromcodes);
+tconv_EXPORT size_t   tconv(tconv_t tconvp, char **inbufpp, size_t *inbytesleftlp, char **outbufpp, size_t *outbytesleftlp);
+tconv_EXPORT int      tconv_close(tconv_t tconvp);
 
 #endif /* TCONV_H */
