8735cc3b3ccfc4d16a4b641b2ef17a7efd707cdf
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-02-23 12:06:49 +0100

    All-in-one and static linking

--- cmake/BuildDependency.cmake
+++ cmake/BuildDependency.cmake
@@ -1,22 +1,11 @@
-MACRO (BuildDependency arg_name arg_srcdir arg_workdir arg_installprefix arg_cflags)
-  #
-  # Keep a copy of previous CFLAGS
-  #
-  IF ($ENV{CFLAGS})
-    SET (BACKUP_CFLAGS $ENV{CFLAGS})
-    SET (HAVE_BACKUP_CFLAGS TRUE)
-  ELSE ()
-    SET (BACKUP_CFLAGS "")
-    SET (HAVE_BACKUP_CFLAGS FALSE)
-  ENDIF ()
-  SET (ENV{CFLAGS} "${BACKUP_CFLAGS} ${${arg_cflags}}")
+MACRO (BuildDependency arg_name arg_srcdir arg_workdir arg_installprefix)
   MESSAGE (STATUS "... Processing dependency ${arg_name}")
   MESSAGE (STATUS "... Copying ${arg_name}")
   MESSAGE (STATUS "... ... ${CMAKE_COMMAND} -E copy_directory ${arg_srcdir} ${arg_workdir}")
   EXECUTE_PROCESS(
     COMMAND "${CMAKE_COMMAND}" -E copy_directory "${arg_srcdir}" "${arg_workdir}"
     )
-  MESSAGE (STATUS "... Setting up ${arg_name}, generator ${CMAKE_GENERATOR}, build type ${CMAKE_BUILD_TYPE}, install prefix ${arg_installprefix}, cflags $ENV{CFLAGS}")
+  MESSAGE (STATUS "... Setting up ${arg_name}, generator ${CMAKE_GENERATOR}, build type ${CMAKE_BUILD_TYPE}, install prefix ${arg_installprefix}")
   EXECUTE_PROCESS(
     COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" . -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${arg_installprefix}
     WORKING_DIRECTORY "${arg_workdir}"
@@ -26,7 +15,4 @@ MACRO (BuildDependency arg_name arg_srcdir arg_workdir arg_installprefix arg_cfl
     COMMAND "${CMAKE_MAKE_PROGRAM}" install
     WORKING_DIRECTORY "${arg_workdir}"
     )
-  IF (HAVE_BACKUP_CFLAGS)
-    SET (ENV{CFLAGS} "$BACKUP_CFLAGS")
-  ENDIF ()
 ENDMACRO ()
