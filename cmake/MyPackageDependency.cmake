f8c5f7904224f71cc57b97c36cac05daa0e83a31
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-03-01 20:07:09 +0100

    MyPackageStart

--- cmake/MyPackageDependency.cmake
+++ cmake/MyPackageDependency.cmake
@@ -1,14 +1,3 @@
-#
-# All /my/ packages follow this convention:
-# - a package Xx has a FindXx's cmake, that will set XX_FOUND, XX_INCLUDE_DIRS, XX_ROOT_DIR variables if success
-#   In case of success XX_LIBRARIES is always XX_ROOT_DIR/lib
-# - there is a target Xx that is the library
-# - there is always an include directory
-# - build always generate headers in output/include
-# - Tests always overwrite PATH with value of variable TEST_PATH
-# - Sourcedir of package Xx is always inside current package as 3rdparty/github/Xx
-# - There all under github at https://github.com/jddurand/c-Xx.git
-#
 MACRO (MYPACKAGEDEPENDENCY dependTarget dependSourceDir dependIncludeDirsVarname)
   STRING (TOUPPER ${dependTarget} _FINDPACKAGE)
   IF (ALL_IN_ONE)
@@ -101,4 +90,86 @@ MACRO (MYPACKAGEDEPENDENCY dependTarget dependSourceDir dependIncludeDirsVarname
     MESSAGE(STATUS "Added ${${_FINDPACKAGE}_RUNTIME_DIRECTORY} to test path")
   ENDIF ()
   MESSAGE(STATUS "Setted ${dependIncludeDirsVarname} to ${${dependIncludeDirsVarname}}")
+ENDMACRO()
+
+MACRO (MYPACKAGESTART packageName versionMajor versionMinor versionPath)
+  STRING (TOUPPER ${packageName} _PACKAGENAME)
+  #
+  # Start
+  #
+  CMAKE_MINIMUM_REQUIRED (VERSION 3.0.0 FATAL_ERROR)
+  PROJECT (${packageName} C CXX)
+  #
+  # Policies
+  #
+  IF (POLICY CMP0063)
+    CMAKE_POLICY (SET CMP0063 NEW)
+  ENDIF ()
+  IF (POLICY CMP0018)
+    CMAKE_POLICY (SET CMP0018 NEW)
+  ENDIF ()
+  #
+  # Options
+  #
+  OPTION (ALL_IN_ONE "Compile non-system wide dependencies locally" OFF)
+  #
+  # We want to be the first one that create check target, so that we own it
+  #
+  IF (NOT TARGET check)
+    ADD_CUSTOM_TARGET (check COMMAND ${CMAKE_CTEST_COMMAND})
+  ENDIF ()
+  #
+  # Use GNUInstallDirs in order to enforce lib64 if needed
+  #
+  INCLUDE (GNUInstallDirs)
+  #
+  # Include system libraries if needed (like on Windows)
+  #
+  INCLUDE (InstallRequiredSystemLibraries)
+  #
+  # Paths
+  #
+  SET (CMAKE_MODULE_PATH      "${PROJECT_SOURCE_DIR}/cmake")           # General module search path
+  SET (INCLUDE_OUTPUT_PATH    "${PROJECT_SOURCE_DIR}/output/include")  # General include output path
+  SET (LIBRARY_OUTPUT_PATH    "${PROJECT_SOURCE_DIR}/output/lib")      # General library output path
+  SET (BINARY_OUTPUT_PATH     "${PROJECT_SOURCE_DIR}/output/bin")      # General binary output path
+  SET (3RDPARTY_OUTPUT_PATH   "${PROJECT_SOURCE_DIR}/output/3rdparty") # General 3rdparty output path
+  #
+  # Output directories
+  # C.f. http://stackoverflow.com/questions/7747857/in-cmake-how-do-i-work-around-the-debug-and-release-directories-visual-studio-2
+  #
+  SET (CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${LIBRARY_OUTPUT_PATH}")
+  SET (CMAKE_LIBRARY_OUTPUT_DIRECTORY "${LIBRARY_OUTPUT_PATH}")
+  SET (CMAKE_RUNTIME_OUTPUT_DIRECTORY "${LIBRARY_OUTPUT_PATH}")
+  FOREACH (OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
+    STRING( TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG )
+    SET ( CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${LIBRARY_OUTPUT_PATH}")
+    SET ( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${LIBRARY_OUTPUT_PATH}")
+    SET ( CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${LIBRARY_OUTPUT_PATH}")
+  ENDFOREACH (OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES)
+  #
+  # Compilers settings
+  #
+  SET (CMAKE_C_VISIBILITY_PRESET   hidden)
+  SET (CMAKE_CXX_VISIBILITY_PRESET hidden)
+  SET (CMAKE_POSITION_INDEPENDENT_CODE ON)
+  if (("${CMAKE_C_COMPILER_ID} x" MATCHES "MSVC") OR MSVC)
+    # Loosely based on http://www.hdfgroup.org/ftp/HDF/HDF_Current/src/unpacked/config/cmake/ConfigureChecks.cmake
+    ADD_DEFINITIONS(-DWIN32_LEAN_AND_MEAN)
+    ADD_DEFINITIONS(-D_CRT_SECURE_NO_WARNINGS)
+    ADD_DEFINITIONS(-D_CRT_NONSTDC_NO_DEPRECATE)
+  ENDIF ()
+  IF ((NOT CMAKE_BUILD_TYPE MATCHES Debug) AND (NOT CMAKE_BUILD_TYPE MATCHES RelWithDebInfo))
+    ADD_DEFINITIONS(-D${_PACKAGENAME}_NTRACE)
+  ENDIF ((NOT CMAKE_BUILD_TYPE MATCHES Debug) AND (NOT CMAKE_BUILD_TYPE MATCHES RelWithDebInfo))
+  SET (${_PACKAGENAME}_VERSION_MAJOR 1)
+  SET (${_PACKAGENAME}_VERSION_MINOR 0)
+  SET (${_PACKAGENAME}_VERSION_PATCH 16)
+  SET (${_PACKAGENAME}_VERSION "${${_PACKAGENAME}_VERSION_MAJOR}.${${_PACKAGENAME}_VERSION_MINOR}.${${_PACKAGENAME}_VERSION_PATCH}")
+
+  ADD_DEFINITIONS(-D${_PACKAGENAME}_VERSION="${${_PACKAGENAME}_VERSION}")
+  #
+  # Prepare dependencies
+  #
+  EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" -E make_directory "${3RDPARTY_OUTPUT_PATH}")
 ENDMACRO()
