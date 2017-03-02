e6f3326ec552b90651d4df346fcff7eafaf3ac93
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-03-02 18:32:10 +0100

    git_subtree_pull.zsh

--- git_subtree_pull.zsh
+++ git_subtree_pull.zsh
@@ -1,7 +1,12 @@
-#!env zsh
+#!zsh -x
 
+git fetch origin
 foreach this (cmake-utils) {
   git fetch $this-remote master
+}
+
+git clean -ffdx
+foreach this (cmake-utils) {
   git subtree pull --prefix 3rdparty/github/$this-remote $this-remote master --squash
 }
 
