54555ad6c369746f00589c663da9559fb847bc8b
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-03-03 06:51:12 +0100

    dev+

--- cmake/MyPackageDependency.cmake
+++ cmake/MyPackageDependency.cmake
@@ -1,85 +1,81 @@
-MACRO (MYPACKAGEDEPENDENCY dependTarget dependSourceDir dependIncludeDirsVarname)
-  STRING (TOUPPER ${dependTarget} _FINDPACKAGE)
-  IF (ALL_IN_ONE)
-    IF (NOT TARGET ${dependTarget})
-      #
-      # Absolute path, otherwise using it as include dir for compilation will croak
-      #
-      GET_FILENAME_COMPONENT(dependSourceDirAbsolute ${dependSourceDir} ABSOLUTE)
-
-      MESSAGE(STATUS "Using ${dependTarget} target from subdirectory ${dependSourceDirAbsolute}")
-      ADD_SUBDIRECTORY(${dependSourceDirAbsolute})
-      SET (${dependIncludeDirsVarname}
-        "${dependSourceDirAbsolute}/output/include"
-        "${dependSourceDirAbsolute}/include"
-        )
-      #
-      # Test path management
-      #
-      SET (_dependLibraryRuntimeDirectory "${dependSourceDirAbsolute}/output/lib")
-      IF ("${CMAKE_HOST_SYSTEM}" MATCHES ".*Windows.*")
-        STRING(REGEX REPLACE "/" "\\\\"  _dependLibraryRuntimeDirectory "${_dependLibraryRuntimeDirectory}")
+MACRO (MYPACKAGEDEPENDENCY packageDepend packageDependSourceDir)
+  #
+  # Get current project name - empty and it is ok if caller was not using MyPackageStart
+  #
+  GET_PROPERTY(_packageName DIRECTORY "${PROJECT_SOURCE_DIR}" PROPERTY MYPACKAGESTART_NAME)
+  IF (NOT ${_packageName})
+    #
+    # Was not using MyPackageStart ? Guess the name from the basename, removing -remote, -surround
+    # if any.
+    #
+    GET_FILENAME_COMPONENT(_packageName ${PROJECT_SOURCE_DIR} NAME)
+    STRING(REGEX REPLACE "-remote.*" ""  _packageName "${_packageName}")
+    STRING(REGEX REPLACE "-surround.*" ""  _packageName "${_packageName}")
+    SET_PROPERTY(_packageName DIRECTORY ${PROJECT_SOURCE_DIR} PROPERTY MYPACKAGESTART_NAME)
+    MESSAGE (WARNING "[${_packageName}] Package name guess - use MyPackageStart to avoid this warning")
+  ENDIF ()
+  #
+  # Check if inclusion was already done - via us or another mechanism... guessed with TARGET check
+  #
+  GET_PROPERTY(_depend GLOBAL PROPERTY "MYPACKAGEDEPENDENCY_${packageDepend}")
+  IF ((${_depend}) OR (TARGET ${packageDepend}))
+    IF (${_depend})
+      IF (${_depend} GREATER 0)
+        MESSAGE (STATUS "[${_packageName}] ${packageDepend} is already being processed")
       ELSE ()
-        STRING(REGEX REPLACE " " "\\\\ "  _dependLibraryRuntimeDirectory "${_dependLibraryRuntimeDirectory}")
+        MESSAGE (STATUS "[${_packageName}] ${packageDepend} is already available")
       ENDIF ()
-      SET(${_FINDPACKAGE}_RUNTIME_DIRECTORY "${_dependLibraryRuntimeDirectory}" CACHE INTERNAL "${dependTarget} Runtime Directory")
-      IF (NOT TEST_PATH)
-        MESSAGE(STATUS "Initializing TEST_PATH with PATH")
-        SET (TEST_PATH "$ENV{PATH}" )
-      ENDIF ()
-      IF ("${CMAKE_HOST_SYSTEM}" MATCHES ".*Windows.*")
-        SET (SEP "\\;")
-      ELSE ()
-        SET (SEP ":")
-      ENDIF ()
-      SET (TEST_PATH "${${_FINDPACKAGE}_RUNTIME_DIRECTORY}${SEP}${TEST_PATH}")
-      MESSAGE(STATUS "Added ${${_FINDPACKAGE}_RUNTIME_DIRECTORY} to test path")
     ELSE ()
-      MESSAGE(STATUS "Re-using already existing ${dependTarget} target")
+      MESSAGE (WARNING "[${_packageName}] Target ${packageDepend} already exist - use MyPackageDependency to avoid this warning")
+      SET_PROPERTY(GLOBAL PROPERTY "MYPACKAGEDEPENDENCY_${packageDepend}" -1)
     ENDIF ()
   ELSE ()
-    MESSAGE(STATUS "Looking for ${dependTarget}")
-    FIND_PACKAGE (${dependTarget})
-    IF (NOT ${_FINDPACKAGE}_FOUND)
-      MESSAGE (FATAL_ERROR "find ${dependTarget} failure")
+    MESSAGE (STATUS "[${_packageName}] ${packageDepend} is not yet available")
+    SET_PROPERTY(GLOBAL PROPERTY "MYPACKAGEDEPENDENCY_${packageDepend}" 1)
+    IF (ALL_IN_ONE)
+      GET_FILENAME_COMPONENT(packageDependSourceDirAbsolute ${packageDependSourceDir} ABSOLUTE)
+      MESSAGE (STATUS "[${_packageName}] Adding subdirectory ${packageDependSourceDirAbsolute}")
+      ADD_SUBDIRECTORY(${packageDependSourceDirAbsolute})
+    ELSE ()
+      MESSAGE(STATUS "[${_packageName}] Looking for ${packageDepend}")
+      FIND_PACKAGE (${packageDepend})
+      STRING (TOUPPER ${packageDepend} _FINDPACKAGE)
+      IF (NOT ${_FINDPACKAGE}_FOUND)
+        MESSAGE (FATAL_ERROR "[${_packageName}] Find ${packageDepend} failed")
+      ENDIF ()
+    ENDIF ()
+    SET_PROPERTY(GLOBAL PROPERTY "MYPACKAGEDEPENDENCY_${packageDepend}" -1)
+    #
+    # Generate the dependencies on the current level, remembering them so that
+    # higher levels inherit
+    #
+    # Include directories of packageDepend
+    #
+    GET_PROPERTY(_include_directories_packageDepend GLOBAL "MYPACKAGEDEPENDENCY_${packageDepend}_INCLUDE_DIRECTORIES")
+    IF (NOT ${_include_directories_packageDepend})
+      SET (_include_directories_packageDepend "${packageDependSourceDirAbsolute}/output/include" "${packageDependSourceDirAbsolute}/include")
+      SET_PROPERTY(GLOBAL "MYPACKAGEDEPENDENCY_${packageDepend}_INCLUDE_DIRECTORIES" ${_include_directories_packageDepend})
     ENDIF ()
     #
-    # The following lines are experimental
+    # Current project's include directories
     #
-    # IF (NOT ${_FINDPACKAGE}_FOUND)
-    #   SET (_GIT_REPOSITORY "https://github.com/jddurand/c-${dependTarget}.git")
-    #   GET_FILENAME_COMPONENT(_INSTALL_DIR "output/3rdparty/${dependTarget}" ABSOLUTE)
-    #    MESSAGE (STATUS "find ${dependTarget} failure - trying with ${_GIT_REPOSITORY}")
-    #    INCLUDE(ExternalProject)
-    #    ExternalProject_Add("${dependTarget}ExternalProject"
-    #      GIT_REPOSITORY ${_GIT_REPOSITORY}
-    #      INSTALL_DIR ${_INSTALL_DIR}
-    #      LOG_DOWNLOAD 1
-    #      LOG_CONFIGURE 1
-    #      LOG_BUILD 1
-    #      LOG_INSTALL 1
-    #      )
-    #    #
-    #    # Cross the fingers that this will work -;
-    #    #
-    #    SET (${_FINDPACKAGE}_ROOT_DIR     ${_INSTALL_DIR})
-    #    SET (${_FINDPACKAGE}_INCLUDE_DIRS "${_INSTALL_DIR}/include")
-    #  ENDIF ()
-    SET (${dependIncludeDirsVarname}  "${${_FINDPACKAGE}_INCLUDE_DIRS}" )
+    FOREACH (_include_directory IN LISTS ${_include_directories_packageDepend})
+      MESSAGE(STATUS "[${_packageName}] Adding include directory ${_include_directory}")
+      INCLUDE_DIRECTORIES(${_include_directory})
+    ENDFOREACH ()
     #
     # Test path management
     #
+    SET (_dependLibraryRuntimeDirectory "${packageDependSourceDirAbsolute}/output/lib")
     IF ("${CMAKE_HOST_SYSTEM}" MATCHES ".*Windows.*")
-      SET(_${_FINDPACKAGE}_RUNTIME_DIRECTORY "${${_FINDPACKAGE}_ROOT_DIR}/bin")
-      STRING(REGEX REPLACE "/" "\\\\"  _${_FINDPACKAGE}_RUNTIME_DIRECTORY "${_${_FINDPACKAGE}_RUNTIME_DIRECTORY}")
+      STRING(REGEX REPLACE "/" "\\\\"  _dependLibraryRuntimeDirectory "${_dependLibraryRuntimeDirectory}")
     ELSE ()
-      SET(_${_FINDPACKAGE}_RUNTIME_DIRECTORY "${${_FINDPACKAGE}_ROOT_DIR}/lib")
-      STRING(REGEX REPLACE " " "\\\\ "  _${_FINDPACKAGE}_RUNTIME_DIRECTORY "${_${_FINDPACKAGE}_RUNTIME_DIRECTORY}")
+      STRING(REGEX REPLACE " " "\\\\ "  _dependLibraryRuntimeDirectory "${_dependLibraryRuntimeDirectory}")
     ENDIF ()
-    SET(${_FINDPACKAGE}_RUNTIME_DIRECTORY "${_${_FINDPACKAGE}_RUNTIME_DIRECTORY}" CACHE INTERNAL "${dependTarget} Runtime Directory")
-    IF (NOT TEST_PATH)
+    SET(${_FINDPACKAGE}_RUNTIME_DIRECTORY "${_dependLibraryRuntimeDirectory}" CACHE INTERNAL "${packageDepend} Runtime Directory")
+    IF (NOT ${TEST_PATH})
       MESSAGE(STATUS "Initializing TEST_PATH with PATH")
-      SET (TEST_PATH "$ENV{PATH}")
+      SET (TEST_PATH "$ENV{PATH}" )
     ENDIF ()
     IF ("${CMAKE_HOST_SYSTEM}" MATCHES ".*Windows.*")
       SET (SEP "\\;")
@@ -89,87 +85,4 @@ MACRO (MYPACKAGEDEPENDENCY dependTarget dependSourceDir dependIncludeDirsVarname
     SET (TEST_PATH "${${_FINDPACKAGE}_RUNTIME_DIRECTORY}${SEP}${TEST_PATH}")
     MESSAGE(STATUS "Added ${${_FINDPACKAGE}_RUNTIME_DIRECTORY} to test path")
   ENDIF ()
-  MESSAGE(STATUS "Setted ${dependIncludeDirsVarname} to ${${dependIncludeDirsVarname}}")
-ENDMACRO()
-
-MACRO (MYPACKAGESTART packageName versionMajor versionMinor versionPath)
-  STRING (TOUPPER ${packageName} _PACKAGENAME)
-  #
-  # Start
-  #
-  CMAKE_MINIMUM_REQUIRED (VERSION 3.0.0 FATAL_ERROR)
-  PROJECT (${packageName} C CXX)
-  #
-  # Policies
-  #
-  IF (POLICY CMP0063)
-    CMAKE_POLICY (SET CMP0063 NEW)
-  ENDIF ()
-  IF (POLICY CMP0018)
-    CMAKE_POLICY (SET CMP0018 NEW)
-  ENDIF ()
-  #
-  # Options
-  #
-  OPTION (ALL_IN_ONE "Compile non-system wide dependencies locally" OFF)
-  #
-  # We want to be the first one that create check target, so that we own it
-  #
-  IF (NOT TARGET check)
-    ADD_CUSTOM_TARGET (check COMMAND ${CMAKE_CTEST_COMMAND})
-  ENDIF ()
-  #
-  # Use GNUInstallDirs in order to enforce lib64 if needed
-  #
-  INCLUDE (GNUInstallDirs)
-  #
-  # Include system libraries if needed (like on Windows)
-  #
-  INCLUDE (InstallRequiredSystemLibraries)
-  #
-  # Paths
-  #
-  SET (CMAKE_MODULE_PATH      "${PROJECT_SOURCE_DIR}/cmake")           # General module search path
-  SET (INCLUDE_OUTPUT_PATH    "${PROJECT_SOURCE_DIR}/output/include")  # General include output path
-  SET (LIBRARY_OUTPUT_PATH    "${PROJECT_SOURCE_DIR}/output/lib")      # General library output path
-  SET (BINARY_OUTPUT_PATH     "${PROJECT_SOURCE_DIR}/output/bin")      # General binary output path
-  SET (3RDPARTY_OUTPUT_PATH   "${PROJECT_SOURCE_DIR}/output/3rdparty") # General 3rdparty output path
-  #
-  # Output directories
-  # C.f. http://stackoverflow.com/questions/7747857/in-cmake-how-do-i-work-around-the-debug-and-release-directories-visual-studio-2
-  #
-  SET (CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${LIBRARY_OUTPUT_PATH}")
-  SET (CMAKE_LIBRARY_OUTPUT_DIRECTORY "${LIBRARY_OUTPUT_PATH}")
-  SET (CMAKE_RUNTIME_OUTPUT_DIRECTORY "${LIBRARY_OUTPUT_PATH}")
-  FOREACH (OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
-    STRING( TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG )
-    SET ( CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${LIBRARY_OUTPUT_PATH}")
-    SET ( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${LIBRARY_OUTPUT_PATH}")
-    SET ( CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${LIBRARY_OUTPUT_PATH}")
-  ENDFOREACH (OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES)
-  #
-  # Compilers settings
-  #
-  SET (CMAKE_C_VISIBILITY_PRESET   hidden)
-  SET (CMAKE_CXX_VISIBILITY_PRESET hidden)
-  SET (CMAKE_POSITION_INDEPENDENT_CODE ON)
-  if (("${CMAKE_C_COMPILER_ID} x" MATCHES "MSVC") OR MSVC)
-    # Loosely based on http://www.hdfgroup.org/ftp/HDF/HDF_Current/src/unpacked/config/cmake/ConfigureChecks.cmake
-    ADD_DEFINITIONS(-DWIN32_LEAN_AND_MEAN)
-    ADD_DEFINITIONS(-D_CRT_SECURE_NO_WARNINGS)
-    ADD_DEFINITIONS(-D_CRT_NONSTDC_NO_DEPRECATE)
-  ENDIF ()
-  IF ((NOT CMAKE_BUILD_TYPE MATCHES Debug) AND (NOT CMAKE_BUILD_TYPE MATCHES RelWithDebInfo))
-    ADD_DEFINITIONS(-D${_PACKAGENAME}_NTRACE)
-  ENDIF ((NOT CMAKE_BUILD_TYPE MATCHES Debug) AND (NOT CMAKE_BUILD_TYPE MATCHES RelWithDebInfo))
-  SET (${_PACKAGENAME}_VERSION_MAJOR 1)
-  SET (${_PACKAGENAME}_VERSION_MINOR 0)
-  SET (${_PACKAGENAME}_VERSION_PATCH 16)
-  SET (${_PACKAGENAME}_VERSION "${${_PACKAGENAME}_VERSION_MAJOR}.${${_PACKAGENAME}_VERSION_MINOR}.${${_PACKAGENAME}_VERSION_PATCH}")
-
-  ADD_DEFINITIONS(-D${_PACKAGENAME}_VERSION="${${_PACKAGENAME}_VERSION}")
-  #
-  # Prepare dependencies
-  #
-  EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" -E make_directory "${3RDPARTY_OUTPUT_PATH}")
 ENDMACRO()
