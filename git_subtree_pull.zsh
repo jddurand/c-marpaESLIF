b68da53d74bf312f1285da5be0e138b1bbf87316
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-03-05 08:17:52 +0100

    git_subtree_pull.zsh [ci skip]

--- git_subtree_pull.zsh
+++ git_subtree_pull.zsh
@@ -1,7 +1,13 @@
-#!env zsh
+#!zsh -x
 
-foreach this (cmake-utils genericHash genericLogger genericSparseArray genericStack) {
+git fetch origin
+foreach this (cmake-utils genericLogger genericStack genericHash genericSparseArray) {
   git fetch $this-remote master
+}
+
+git reset --hard origin/master
+git clean -ffdx
+foreach this (cmake-utils genericLogger genericStack genericHash genericSparseArray) {
   git subtree pull --prefix 3rdparty/github/$this-remote $this-remote master --squash
 }
 
