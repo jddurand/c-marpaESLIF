b48ad3d2088b6a4f46cd483f46d6d337f6984149
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-03-06 21:07:40 +0100

    git_subtree_pull.zsh

--- git_subtree_pull.zsh
+++ git_subtree_pull.zsh
@@ -5,7 +5,7 @@ foreach this (cmake-utils genericLogger genericStack tconv marpaWrapper) {
   git fetch $this-remote master
 }
 
-git reset --hard origin/master
+# git reset --hard origin/master
 git clean -ffdx
 foreach this (cmake-utils genericLogger genericStack tconv marpaWrapper) {
   git subtree pull --prefix 3rdparty/github/$this-remote $this-remote master --squash
