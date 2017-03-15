e43610d2d289a697cbf9b7deb52124213fba99f6
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-03-15 15:17:30 +0100

    Renamings

--- git_subtree_pull.zsh
+++ git_subtree_pull.zsh
@@ -1,14 +1,14 @@
 #!zsh -x
 
 git fetch origin
-foreach this (cmake-utils genericLogger genericStack genericHash genericSparseArray) {
-  git fetch $this-remote master
+foreach this (cmake-utils genericLogger genericStack genericHash genericSparseArray Marpa--R2) {
+  git fetch $this master
 }
 
 git reset --hard origin/master
 git clean -ffdx
 foreach this (cmake-utils genericLogger genericStack genericHash genericSparseArray) {
-  git subtree pull --prefix 3rdparty/github/$this-remote $this-remote master --squash
+  git subtree pull --prefix 3rdparty/github/$this $this master --squash
 }
 
 exit 0
