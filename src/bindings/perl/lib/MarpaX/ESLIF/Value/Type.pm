8aa8464107ca6ee3d688cf521abb5fef1997a467
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-04-05 16:20:39 +0200

    Value/Type.pm: POD [ci skip]

--- src/bindings/perl/lib/MarpaX/ESLIF/Value/Type.pm
+++ src/bindings/perl/lib/MarpaX/ESLIF/Value/Type.pm
@@ -9,7 +9,7 @@ package MarpaX::ESLIF::Value::Type;
 
 =head1 DESCRIPTION
 
-ESLIF values are mapped to constants. This module is giving access to them.
+ESLIF values are mapped to constants. This module is giving access to them, although this will never be visible from perl interface.
 
 =head1 SYNOPSIS
 
@@ -31,6 +31,54 @@ ESLIF values are mapped to constants. This module is giving access to them.
 use Carp qw/croak/;
 
 # VERSION
+
+=head1 CONSTANTS
+
+=head2 MARPAESLIF_VALUE_TYPE_UNDEF
+
+Undefined value.
+
+=head2 MARPAESLIF_VALUE_TYPE_CHAR
+
+I<C>'s C<char>.
+
+=head2 MARPAESLIF_VALUE_TYPE_SHORT
+
+I<C>'s C<short>.
+
+=head2 MARPAESLIF_VALUE_TYPE_INT
+
+I<C>'s C<int>.
+
+=head2 MARPAESLIF_VALUE_TYPE_LONG
+
+I<C>'s C<long>.
+
+=head2 MARPAESLIF_VALUE_TYPE_FLOAT
+
+I<C>'s C<float>.
+
+=head2 MARPAESLIF_VALUE_TYPE_DOUBLE
+
+I<C>'s C<double>.
+
+=head2 MARPAESLIF_VALUE_TYPE_PTR
+
+I<C>'s C<void *>.
+
+=head2 MARPAESLIF_VALUE_TYPE_PTR_SHALLOW
+
+I<C>'s C<void *> where the later is a shallow copy.
+
+=head2 MARPAESLIF_VALUE_TYPE_ARRAY
+
+I<C>'s pointer to a C<{void *, size_t}> structure.
+
+=head2 MARPAESLIF_VALUE_TYPE_ARRAY_SHALLOW
+
+I<C>'s pointer to a C<{void *, size_t}> structure where C<void *> is a shallow copy.
+
+=cut
 
 # This section should be replaced on-the-fly at build time
 # AUTOLOAD
