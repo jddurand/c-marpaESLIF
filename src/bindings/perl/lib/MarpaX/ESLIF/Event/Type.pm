3a9e4376fe28fa873920a167e1eb76d21387184c
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-04-07 15:04:38 +0200

    Perl - re index Event::Type [ci skip]

--- src/bindings/perl/lib/MarpaX/ESLIF/Event/Type.pm
+++ src/bindings/perl/lib/MarpaX/ESLIF/Event/Type.pm
@@ -9,7 +9,7 @@ package MarpaX::ESLIF::Event::Type;
 
 =head1 DESCRIPTION
 
-ESLIF events are mapped to constants. This module is giving access to them, although they have no use in the perl interface.
+ESLIF events are mapped to constants. This module is giving access to them.
 
 =head1 SYNOPSIS
 
