6969522debf21f486f5befb8302ca392098cd493
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-04-12 20:45:03 +0200

    Fix constant synopsis [ci skip]

--- src/bindings/perl/lib/MarpaX/ESLIF/Value/Type.pm
+++ src/bindings/perl/lib/MarpaX/ESLIF/Value/Type.pm
@@ -14,17 +14,17 @@ ESLIF values are mapped to constants. This module is giving access to them, alth
 =head1 SYNOPSIS
 
   my $valueType;
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_UNDEF;         #  0
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_CHAR;          #  1
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_SHORT;         #  2
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_INT;           #  3
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_LONG;          #  4
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_FLOAT;         #  5
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_DOUBLE;        #  6
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_PTR;           #  7
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_PTR_SHALLOW;   #  8
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_ARRAY;         #  9
-  $valueType = MarpaX::ESLIF::Value::Type::MARPAESLIF_VALUE_TYPE_ARRAY_SHALLOW; # 10
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_UNDEF;         #  0
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_CHAR;          #  1
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_SHORT;         #  2
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_INT;           #  3
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_LONG;          #  4
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_FLOAT;         #  5
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_DOUBLE;        #  6
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_PTR;           #  7
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_PTR_SHALLOW;   #  8
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_ARRAY;         #  9
+  $valueType = $MarpaX::ESLIF::Value::Type->MARPAESLIF_VALUE_TYPE_ARRAY_SHALLOW; # 10
 
 =cut
 
