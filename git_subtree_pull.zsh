7f0e5796791fd4c75f197986ac225d113bbbf81a
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-05-08 07:22:42 +0200

    git_subtree_pull.zsh: Added genericHash [ci skip]

--- git_subtree_pull.zsh
+++ git_subtree_pull.zsh
@@ -1,13 +1,13 @@
 #!zsh -x
 
 git fetch origin
-foreach this (cmake-utils genericLogger genericStack tconv marpaWrapper) {
+foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper) {
   git fetch $this master
 }
 
 # git reset --hard origin/master
 git clean -ffdx
-foreach this (cmake-utils genericLogger genericStack tconv marpaWrapper) {
+foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper) {
   git subtree pull --prefix 3rdparty/github/$this $this master --squash
 }
 
