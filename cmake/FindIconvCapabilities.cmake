abeccbdb781735d85e4a23b8f896a8c0438c562f
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-05-09 11:40:23 +0200

    cmake/FindIconvCapabilities.cmake revisit [ci skip]

--- cmake/FindIconvCapabilities.cmake
+++ cmake/FindIconvCapabilities.cmake
@@ -4,101 +4,93 @@
 # ICONV_CAN_TRANSLIT      - iconv supports //TRANSLIT option
 # ICONV_CAN_IGNORE        - iconv supports //IGNORE option
 # 
-include(CheckCSourceCompiles)
 
-SET (CMAKE_REQUIRED_INCLUDES_PREVIOUS ${CMAKE_REQUIRED_INCLUDES})
-SET (CMAKE_REQUIRED_LIBRARIES_PREVIOUS ${CMAKE_REQUIRED_LIBRARIES})
-SET (CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
-SET (CMAKE_REQUIRED_LIBRARIES ${ICONV_LINK_FLAGS})
-
-MESSAGE(STATUS "Checking if iconv supports identical charsets in input")
-CHECK_C_SOURCE_COMPILES ("
-  #include <iconv.h>
-  int main(){
-    iconv_t conv;
-
-    conv = iconv_open(\"ASCII\", \"ASCII\");
-    return (conv == (iconv_t)-1) ? 1 : 0;
-  }
-" ICONV_CAN_SAME_CHARSETS )
-IF (NOT ICONV_CAN_SAME_CHARSETS)
-  MESSAGE (WARNING "When input and destination charsets are equivalent, they will NOT be checked and fuzzy conversion will happen")
-ENDIF ()
-
-MESSAGE(STATUS "Checking if iconv supports //TRANSLIT option")
-CHECK_C_SOURCE_COMPILES ("
-  #include <iconv.h>
-  int main(){
-    iconv_t conv;
-
-    conv = iconv_open(\"ASCII//TRANSLIT\", \"UTF-8\");
-    return (conv == (iconv_t)-1) ? 1 : 0;
-  }
-" ICONV_CAN_TRANSLIT )
-IF (NOT ICONV_CAN_TRANSLIT)
-  MESSAGE(STATUS "Checking if iconv //TRANSLIT option can be ignored")
-  #
-  # We expect a replacement character to have happened automatically, then
-  #
-  CHECK_C_SOURCE_COMPILES ("
+#
+# Just to be sure, we do not limit the calls to iconv_open, but do the full thing
+# Input is FORCED to be UTF-8 in any case. Caller have the choice of destination charset
+# and have to provide TWO TRAILING BYTES.
+#
+MACRO(_FINDICONVCAPABILITY tocharset outputvariable twobytes)
+  include(CheckCSourceRuns)
+  SET (CMAKE_REQUIRED_INCLUDES_PREVIOUS ${CMAKE_REQUIRED_INCLUDES})
+  SET (CMAKE_REQUIRED_LIBRARIES_PREVIOUS ${CMAKE_REQUIRED_LIBRARIES})
+  SET (CMAKE_REQUIRED_DEFINITIONS_PREVIOUS ${CMAKE_REQUIRED_DEFINITIONS})
+  SET (CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES_PREVIOUS} ${ICONV_INCLUDE_DIR})
+  SET (CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES_PREVIOUS} ${ICONV_LINK_FLAGS})
+  IF (WINICONV)
+    SET (CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS_PREVIOUS} -DWINICONV=1)
+  ELSE ()
+    SET (CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS_PREVIOUS} -DWINICONV=0)
+  ENDIF ()
+  CHECK_C_SOURCE_RUNS ("
   #include <stdlib.h>
   #include <stdio.h>
   #include <stddef.h>
+  #if WINICONV
+  #include <win_iconv.c>
+  #endif
   #include <iconv.h>
   int main(){
     iconv_t  conv;
-    char    *src = \"123\\\\xE2\\\\x82\\\\xAC\" \"456\"; /* In UTF8: 123, euro sign, 456 */
+    char    *src = \"123\\\\xE2\\\\x82\\\\xAC\" \"456\" ${twobytes}; /* In UTF8: 123, euro sign, 456 */
     char     dst[1024];
     char    *inp = src;                             /* Input moving pointer */
     char    *outp = dst;                            /* Output moving pointer */
-    size_t   inl = 9;                               /* In UTF8: 9 bytes remaining */
+    size_t   inl = 11;                              /* In UTF8: 9 bytes OK, 2 bytes that the caller is setting */
     size_t   outl = 1024;                           /* 1024 bytes available in output buffer */
-    int      i;
 
-    conv = iconv_open(\"ASCII\", \"UTF-8\");
+    memset((void *) dst, '\\\\0', 1024);
+    conv = iconv_open(\"${tocharset}\", \"UTF-8\");
     if (conv == (iconv_t)-1) { perror(\"iconv_open\"); return 1; }
     if (iconv(conv, &inp, &inl, &outp, &outl) == (size_t)-1)  { perror(\"iconv\"); return 1; }
     /* Flush */
     if (iconv(conv, NULL, NULL, &outp, &outl) == (size_t)-1)  { perror(\"iconv\"); return 1; }
     if (iconv_close(conv) == -1)  { perror(\"iconv_close\"); return 1; }
-    outp = &(dst[3]);
-    while (*++outp != '4');
-    *outp = '\\\\0';
-    outp = &(dst[3]);
-    fprintf(stdout, \"Euro character \\\\\\\\xE2\\\\\\\\x82\\\\\\\\xAC has been transformed to %s\\\\n\", outp);
+    fprintf(stdout, \"%s\\\\n\", dst);
     return 0;
   }
-" ICONV_TRANSLIT_CAN_BE_IGNORED )
-ELSE ()
-  SET (ICONV_TRANSLIT_CAN_BE_IGNORED FALSE)
+" ${outputvariable} )
+  SET (CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES_PREVIOUS})
+  SET (CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES_PREVIOUS})
+  SET (CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS_PREVIOUS})
+  #
+  # Just make sure this is a boolean
+  #
+  IF (${outputvariable})
+    SET (${outputvariable} TRUE)
+  ELSE ()
+    SET (${outputvariable} FALSE)
+  ENDIF ()
+ENDMACRO()
+
+
+SET (TWO_UTF8_BYTES_OK "\"!!\"")
+SET (TWO_UTF8_BYTES_KO "\"\\\\\\\\xa0\\\\\\\\xa1\"")
+
+_FINDICONVCAPABILITY("UTF-8"           ICONV_CAN_SAME_CHARSETS      ${TWO_UTF8_BYTES_OK})
+_FINDICONVCAPABILITY("ASCII//TRANSLIT" ICONV_CAN_TRANSLIT           ${TWO_UTF8_BYTES_OK})
+_FINDICONVCAPABILITY("ASCII"           ICONV_WITHOUT_TRANSLIT_IS_OK ${TWO_UTF8_BYTES_OK})
+_FINDICONVCAPABILITY("ASCII//IGNORE"   ICONV_CAN_IGNORE             ${TWO_UTF8_BYTES_KO})
+_FINDICONVCAPABILITY("ASCII"           ICONV_WITHOUT_IGNORE_IS_OK   ${TWO_UTF8_BYTES_KO})
+
+IF (NOT ICONV_CAN_SAME_CHARSETS)
+  MESSAGE (WARNING "When input and destination charsets are equivalent, they will NOT be checked and fuzzy conversion will happen")
 ENDIF ()
-
-MESSAGE(STATUS "Checking if iconv supports //IGNORE option")
-CHECK_C_SOURCE_COMPILES ("
-  #include <iconv.h>
-  int main(){
-    iconv_t conv;
-
-    conv = iconv_open(\"ASCII//IGNORE\", \"UTF-8\");
-    return (conv == (iconv_t)-1) ? 1 : 0;
-  }
-" ICONV_CAN_IGNORE )
-
-SET (CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES_PREVIOUS})
-SET (CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES_PREVIOUS})
 
 MESSAGE(STATUS "-----------------------------------------")
 MESSAGE(STATUS "Iconv capabilities:")
 MESSAGE(STATUS "")
 MESSAGE(STATUS "      ICONV_CAN_SAME_CHARSETS: ${ICONV_CAN_SAME_CHARSETS}")
 MESSAGE(STATUS "           ICONV_CAN_TRANSLIT: ${ICONV_CAN_TRANSLIT}")
-MESSAGE(STATUS "ICONV_TRANSLIT_CAN_BE_IGNORED: ${ICONV_TRANSLIT_CAN_BE_IGNORED}")
+MESSAGE(STATUS " ICONV_WITHOUT_TRANSLIT_IS_OK: ${ICONV_WITHOUT_TRANSLIT_IS_OK}")
 MESSAGE(STATUS "             ICONV_CAN_IGNORE: ${ICONV_CAN_IGNORE}")
+MESSAGE(STATUS "   ICONV_WITHOUT_IGNORE_IS_OK: ${ICONV_WITHOUT_IGNORE_IS_OK}")
 MESSAGE(STATUS "-----------------------------------------")
 
 MARK_AS_ADVANCED(
   ICONV_CAN_SAME_CHARSETS
   ICONV_CAN_TRANSLIT
-  ICONV_TRANSLIT_CAN_BE_IGNORED
+  ICONV_WITHOUT_TRANSLIT_IS_OK
   ICONV_CAN_IGNORE
+  ICONV_WITHOUT_IGNORE_IS_OK
   )
