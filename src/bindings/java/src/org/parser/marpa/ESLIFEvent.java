67035c06c9da5f431e54838f460aa745a58deb7a
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-08-10 06:23:09 +0200

    Generated equals, hashCode and toString java methods for ESLIFEvent [ci skip]

--- src/bindings/java/src/org/parser/marpa/ESLIFEvent.java
+++ src/bindings/java/src/org/parser/marpa/ESLIFEvent.java
@@ -23,6 +23,62 @@ public class ESLIFEvent {
 		this.event  = event;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((event == null) ? 0 : event.hashCode());
+		result = prime * result + ((symbol == null) ? 0 : symbol.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof ESLIFEvent)) {
+			return false;
+		}
+		ESLIFEvent other = (ESLIFEvent) obj;
+		if (event == null) {
+			if (other.event != null) {
+				return false;
+			}
+		} else if (!event.equals(other.event)) {
+			return false;
+		}
+		if (symbol == null) {
+			if (other.symbol != null) {
+				return false;
+			}
+		} else if (!symbol.equals(other.symbol)) {
+			return false;
+		}
+		if (type != other.type) {
+			return false;
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "ESLIFEvent [type=" + type + ", symbol=" + symbol + ", event=" + event + "]";
+	}
+
 	/**
 	 * 
 	 * @return the event type
