f17fc0ac37f96c8b3bc07cf293d788721d86e222
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-11-23 21:52:17 +0100

    Squashed '3rdparty/github/genericStack/' changes from 658a9e5da..5ef9f575a
    5ef9f575a Version 1.0.44 [ci skip]
    cd9d66172 Merge commit '3e38bf108b3fd9ae137eaeddbee09161ac2a68dd'
    3e38bf108 Squashed '3rdparty/github/cmake-utils/' changes from 3019a08..1dddd0d
    7e6dd7b29 Version 1.0.43
    0b7513f88 Squashed '3rdparty/github/cmake-utils/' changes from 620edd7..3019a08
    fb8fa33f6 Merge commit '0b7513f88b4ed737607324543f2bd589207c553d'
    
    git-subtree-dir: 3rdparty/github/genericStack
    git-subtree-split: 5ef9f575a828cef4bb3d561aeed180a224223c99

--- 3rdparty/github/cmake-utils/cmake/MyPackageMan.cmake
+++ 3rdparty/github/cmake-utils/cmake/MyPackageMan.cmake
@@ -16,8 +16,11 @@ MACRO(MyPackageMan PODFILE MANFILE SECTION VERSION)
     IF (POD2MAN)
       FIND_PROGRAM (GZIP gzip)
       IF (GZIP)
-        SET (MANPAGE_TARGET "man-${MANFILE}")
+        SET (MANPAGE_TARGET "man_${MANFILE}")
         IF (NOT TARGET ${MANPAGE_TARGET})
+          IF (MYPACKAGE_DEBUG)
+            MESSAGE (STATUS "[${PROJECT_NAME}-MAN-DEBUG] Creating target ${MANPAGE_TARGET}")
+          ENDIF ()
           ADD_CUSTOM_COMMAND(
 	    OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${MANFILE}.${SECTION}"
 	    DEPENDS "${PODFILE_ABSOLUTE}"
@@ -34,7 +37,13 @@ MACRO(MyPackageMan PODFILE MANFILE SECTION VERSION)
           INSTALL(
 	    FILES "${CMAKE_CURRENT_BINARY_DIR}/${MANFILE}.${SECTION}.gz"
 	    DESTINATION "share/man/man${SECTION}"
+            COMPONENT ManpageComponent
 	    )
+          SET (_HAVE_MANPAGECOMPONENT TRUE CACHE INTERNAL "Have ManpageComponent" FORCE)
+        ELSE ()
+          IF (MYPACKAGE_DEBUG)
+            MESSAGE (STATUS "[${PROJECT_NAME}-MAN-DEBUG] Target ${MANPAGE_TARGET} already exist")
+          ENDIF ()
         ENDIF ()
       ENDIF ()
     ENDIF ()
