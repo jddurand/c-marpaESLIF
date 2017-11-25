737b9349cd572f1199b25771e8e7ca56a4db50f2
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-11-25 15:52:33 +0100

    Squashed '3rdparty/github/marpaWrapper/' changes from 47dc7441..fcf108ae
    fcf108ae Version 1.0.55
    1e8ade7a Merge commit 'b8996cb1781c712ed4ce879893b6ea5228bbe8c1'
    b8996cb1 Squashed '3rdparty/github/genericSparseArray/' changes from 79c372035..3ab8b0c7b
    1428a363 Merge commit '579a3d03b155ff53dca7a0c9eac67e5aee42c77f'
    579a3d03 Squashed '3rdparty/github/genericHash/' changes from fe555c1e2..6b543e9a3
    d5729e87 Merge commit '39961e0b9af56d77f9e3b363ccbcf8d0caf20189'
    39961e0b Squashed '3rdparty/github/genericStack/' changes from 6c1a46d70..ecaddd5c4
    4191d41e Merge commit '4e67081b9652d04c217bd4d0d41b85e744507554'
    4e67081b Squashed '3rdparty/github/genericLogger/' changes from 3d2d3944d..5c1a59818
    38e44875 Merge commit '79a83063894be57d096a26ea3c4ea21b6c3fb94a'
    79a83063 Squashed '3rdparty/github/cmake-utils/' changes from 7ed20cca8..6aca3dc6c
    ab104292 Fix license [ci skip]
    d3255274 Squashed '3rdparty/github/genericSparseArray/' changes from 3632dd1c1..79c372035
    7aaf3a29 Merge commit 'd32552746bc435cc056323ca06a6ab6e3879f964' [ci skip]
    5fc18f2d Squashed '3rdparty/github/genericHash/' changes from 7bed850c9..fe555c1e2
    02ebf566 Merge commit '5fc18f2dcd286676d6f928321d82bd9d3cce1d14' [ci skip]
    1d18efdf Squashed '3rdparty/github/genericStack/' changes from 5ef9f575a..6c1a46d70
    ecfaaeea Merge commit '1d18efdf11ffd56d3b58c99a5e6ca36343bee279' [ci skip]
    c1b9f894 Squashed '3rdparty/github/genericLogger/' changes from a87197703..3d2d3944d
    68a12997 Merge commit 'c1b9f894f5e82b0f8b9f0e4e91283433e6e4344e' [ci skip]
    8353cf3b Squashed '3rdparty/github/cmake-utils/' changes from 1dddd0db9..7ed20cca8
    0140ef45 Merge commit '8353cf3b3c3b7c2a50ce1e585f03b492b62594c0' [ci skip]
    
    git-subtree-dir: 3rdparty/github/marpaWrapper
    git-subtree-split: fcf108ae553b0c2a55ec4edb1eaec331ada84129

--- 3rdparty/github/genericSparseArray/git_subtree_pull.zsh
+++ 3rdparty/github/genericSparseArray/git_subtree_pull.zsh
@@ -5,6 +5,7 @@ foreach this (cmake-utils genericHash genericLogger) {
   git fetch $this master
 }
 
+git reset --hard origin/master
 git clean -ffdx
 foreach this (cmake-utils genericHash genericLogger) {
   git subtree pull --prefix 3rdparty/github/$this $this master --squash
