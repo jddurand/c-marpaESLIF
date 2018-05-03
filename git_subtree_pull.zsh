b932c3d9ab5225c48b541a394c1a15bd8c729007
Jean-Damien Durand <jeandamiendurand@free.fr>
2018-05-03 07:03:36 +0200

    Add lua534 to git_subtree_pull.zsh [ci skip]

--- git_subtree_pull.zsh
+++ git_subtree_pull.zsh
@@ -1,13 +1,13 @@
 #!zsh -x
 
 git fetch origin
-foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper) {
+foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper lua534) {
   git fetch $this master
 }
 
 git reset --hard origin/master
 git clean -ffdx
-foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper) {
+foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper lua534) {
   git subtree pull --prefix 3rdparty/github/$this $this master --squash
 }
 
