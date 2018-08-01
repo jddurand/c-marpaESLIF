bdd2a1ad18e426d6a87637a0194b623f7cc63559
Jean-Damien Durand <jeandamiendurand@free.fr>
2018-08-01 18:46:03 +0200

    doc/Tutorial/propositionalLogic/propositionalLogicUsingLua.sh

---
+++ doc/Tutorial/propositionalLogic/propositionalLogicUsingLua.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+pushd /home/jdurand/git/c-marpaESLIF
+make marpaESLIF
+popd
+
+cc -I include -I output/include -I 3rdparty/github/genericLogger/include -I 3rdparty/github/genericLogger/output/include -o /tmp/propositionalLogicUsingLua doc/Tutorial/propositionalLogic/propositionalLogicUsingLua.c -L output/lib -lmarpaESLIF -L 3rdparty/github/genericLogger/output/lib -lgenericLogger -lm && LD_LIBRARY_PATH=`pwd`/output/lib:`pwd`/3rdparty/github/genericLogger/output/lib /tmp/propositionalLogicUsingLua
