ced292bc535902c04b064a60f234b30036cac893
Jean-Damien Durand <jeandamiendurand@free.fr>
2018-09-08 10:39:09 +0200

    include/marpaESLIFLua.h: Add "lua.h" [ci skip]

--- src/bindings/lua/include/marpaESLIFLua.h
+++ src/bindings/lua/include/marpaESLIFLua.h
@@ -1,12 +1,13 @@
 #ifndef MARPAESLIFLUA_H
 #define MARPAESLIFLUA_H
 
+#include "lua.h"         /* And not <lua.h> because it is not standardized as per CMake recommandation */
 #include <marpaESLIFLua/export.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-  marpaESLIFLua_EXPORT int luaopen_marpaESLIFLua(lua_State *h);
+  marpaESLIFLua_EXPORT int luaopen_marpaESLIFLua(lua_State *L);
 #ifdef __cplusplus
 }
 #endif
