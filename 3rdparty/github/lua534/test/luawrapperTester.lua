2b3de0f292bdd69749f1647b800ecc0523d927a7
Jean-Damien Durand <jeandamiendurand@free.fr>
2018-11-03 19:49:55 +0100

    lua534 -> luaunpanic

--- 3rdparty/github/lua534/test/luawrapperTester.lua
+++
@@ -1,5 +0,0 @@
--- add two numbers
-
-function add ( x, y )
-	return x + y
-end
--- git_subtree_pull.zsh
+++ git_subtree_pull.zsh
@@ -11,13 +11,13 @@ if [ $havemodif -ne 0 ]; then
 fi
 
 git fetch origin
-foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper lua534) {
+foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper luaunpanic) {
   git fetch $this master
 }
 
 git reset --hard origin/master
 git clean -ffdx
-foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper lua534) {
+foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper luaunpanic) {
   git subtree pull --prefix 3rdparty/github/$this $this master --squash
 }
 
