f204545b62c296a9dcd1efaa4ec310c53215a919
Jean-Damien Durand <jeandamiendurand@free.fr>
2019-01-27 08:28:02 +0100

    internal/structures.h: Increase internal size of generated float number formats [ci skip]

--- include/marpaESLIF/internal/structures.h
+++ include/marpaESLIF/internal/structures.h
@@ -241,9 +241,9 @@ struct marpaESLIF {
   marpaESLIFValueResult_t marpaESLIFValueResultTrue;   /* Pre-filled ::true value result */
   marpaESLIFValueResult_t marpaESLIFValueResultFalse;  /* Pre-filled ::true value result */
   marpaESLIFValueResult_t marpaESLIFValueResultIntZero; /* Pre-filled ::int zero value result */
-  char                    float_fmts[128];             /* Pre-filled format string for floats */
-  char                    double_fmts[128];            /* Pre-filled format string for double */
-  char                    long_double_fmts[128];       /* Pre-filled format string for double */
+  char                    float_fmts[2048];            /* Pre-filled format string for floats */
+  char                    double_fmts[2048];           /* Pre-filled format string for double */
+  char                    long_double_fmts[2048];      /* Pre-filled format string for double */
 };
 
 struct marpaESLIFGrammar {
