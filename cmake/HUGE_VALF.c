d1e304cbdb7ddcefe75102f48a3fa4ad0ded6b08
Jean-Damien Durand <jeandamiendurand@free.fr>
2019-12-18 06:22:52 +0100

    Squashed '3rdparty/github/cmake-utils/' changes from 77c30aef..21673367
    21673367 Fix REPLACEMENT_ by REPLACEMENT ...
    
    git-subtree-dir: 3rdparty/github/cmake-utils
    git-subtree-split: 21673367009260c9232f753efbe7b450660493d5

--- cmake/HUGE_VALF.c
+++ cmake/HUGE_VALF.c
@@ -1,7 +1,7 @@
 #if HAVE_MATH_H
 #include <math.h>
 #endif
-#ifdef HAVE_HUGE_VALF_REPLACEMENT_
+#ifdef HAVE_HUGE_VALF_REPLACEMENT
 #  undef HUGE_VALF
 #  define HUGE_VALF (__builtin_huge_valf())
 #endif
--- cmake/HUGE_VALL.c
+++ cmake/HUGE_VALL.c
@@ -1,7 +1,7 @@
 #if HAVE_MATH_H
 #include <math.h>
 #endif
-#ifdef HAVE_HUGE_VALL_REPLACEMENT_
+#ifdef HAVE_HUGE_VALL_REPLACEMENT
 #  undef HUGE_VALL
 #  define HUGE_VALL (__builtin_huge_vall())
 #endif
