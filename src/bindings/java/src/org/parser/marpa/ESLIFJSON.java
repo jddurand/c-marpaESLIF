a3766995c7b8b0489d9e26269b71bd9bbc628884
Jean-Damien Durand <jeandamiendurand@free.fr>
2020-02-17 07:27:10 +0100

    Java JSON dev+

---
+++ src/bindings/java/src/org/parser/marpa/ESLIFJSON.java
@@ -0,0 +1,96 @@
+package org.parser.marpa;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+/**
+ * ESLIFJSON is a built-in ESLIFGrammar, targetting the JSON syntax, with an optional strict mode.
+ *
+ * Non-strict mode supports:
+ * - Unlimited commas
+ * - Trailing separator
+ * - Perl style comment
+ * - C++ style comment
+ * - InfinityUnlimited commas
+ * - NaN
+ * - Unicode's control characters (range C<[\x00-\x1F]>).
+ * 
+ * <pre>
+ * ESLIF eslif = new ESLIF(...)
+ * ESLIFJSON eslifJSON = new ESLIFJSON();
+ * ...
+ * eslifJSON.encode(someObject);
+ * eslifJSON.decode("\"JSON string\"");
+ * ...
+ * eslifJSON.free();
+ * eslif.free()
+ * </pre>
+ */
+public class ESLIFJSON {
+	private ESLIFJSONDecoder eslifJSONDecoder;
+	private ESLIFJSONEncoder eslifJSONEncoder;
+
+	/*
+	 * ********************************************
+	 * Public methods
+	 * ********************************************
+	 */
+	/**
+	 * Creation of an ESLIFJSON instance
+	 * 
+	 * @param eslif an instance of ESLIF
+	 * @param strict optional strict mode
+	 * @throws ESLIFException if creation failed
+	 * 
+	 * default strict mode is true.
+	 */
+	public ESLIFJSON(ESLIF eslif, boolean strict) throws ESLIFException {
+		this.eslifJSONDecoder = new ESLIFJSONDecoder(eslif, strict);
+		this.eslifJSONEncoder = new ESLIFJSONEncoder(eslif, strict);
+	}
+
+	public ESLIFJSON(ESLIF eslif) throws ESLIFException {
+		this(eslif, true);
+	}
+
+	/**
+	 * Dispose of an ESLIFJSONDecoder resource.
+	 * 
+	 * @throws ESLIFException if the interface failed
+	 */
+	public synchronized void free() throws ESLIFException {
+		this.eslifJSONDecoder.free();
+		this.eslifJSONEncoder.free();
+	}
+
+	/**
+	 * Decodes a string to a java object
+	 *
+	 * @param s the string to decode
+	 * @throws ESLIFException if the interface failed
+	 */
+	public synchronized Object decode(String s) throws ESLIFException {
+		return this.eslifJSONDecoder.decode(s);
+	}
+
+	/**
+	 * Decodes a string to a java object
+	 *
+	 * @param s the string to decode
+	 * @param eslifJSONDeoderOption the options for decoding
+	 * @throws ESLIFException if the interface failed
+	 */
+	public synchronized Object decode(String s, ESLIFJSONDecoderOption eslifJSONDecoderOption) throws ESLIFException {
+		return this.eslifJSONDecoder.decode(s, eslifJSONDecoderOption);
+	}
+
+	/**
+	 * Encodes a java object to a JSON string
+	 *
+	 * @param o the object to encode
+	 * @throws ESLIFException if the interface failed
+	 */
+	public synchronized String encode(Object o) throws ESLIFException {
+		return this.eslifJSONEncoder.encode(o);
+	}
+}
---
+++ src/bindings/java/test/org/parser/marpa/AppJSON.java
@@ -0,0 +1,155 @@
+package org.parser.marpa;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Import/export Application
+ */
+public class AppJSON implements Runnable {
+	ESLIFLoggerInterface eslifLogger;
+	
+	/**
+	 * @param eslifLogger logger interface
+	 */
+	public AppJSON(ESLIFLoggerInterface eslifLogger) {
+		this.eslifLogger = eslifLogger;
+	}
+
+	public void run() {
+		ESLIF eslif;
+		try {
+			eslif = ESLIF.getInstance(eslifLogger);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		if (false) {
+			eslifLogger.info(" ATTACH ME");
+			try {
+				TimeUnit.SECONDS.sleep(10);
+			} catch (InterruptedException e) {
+				System.err.println(e);
+			}
+		}
+
+		String[] strings = {
+						"     [ " +
+						"       { " +
+						"          \"precision\": \"zip\", " +
+						"          \"Latitude\":  37.7668, " +
+						"          \"Longitude\": -122.3959, " +
+						"          \"Address\":   \"\", " +
+						"          \"City\":      \"SAN FRANCISCO\", " +
+						"          \"State\":     \"CA\", " +
+						"          \"Zip\":       \"94107\", " +
+						"          \"Country\":   \"US\" " +
+						"       }, " +
+						"       { " +
+						"          \"precision\": \"zip\", " +
+						"          \"Latitude\":  37.371991, " +
+						"          \"Longitude\": -122.026020, " +
+						"          \"Address\":   \"\", " +
+						"          \"City\":      \"SUNNYVALE\", " +
+						"          \"State\":     \"CA\", " +
+						"          \"Zip\":       \"94085\", " +
+						"          \"Country\":   \"US\" " +
+						"       }, " +
+						"       { " +
+						"          \"+Infinity\": +Infinity, " +
+						"          \"-Infinity\": -Infinity, " +
+						"          \"NaN\":  NaN " +
+						"       } " +
+						"     ] " +
+						" "
+						};
+
+		HashMap<String, Object> hmap = new HashMap<String, Object>();
+		hmap.put("one", "one");
+		hmap.put("two", "two");
+		hmap.put("true", true);
+		hmap.put("false", false);
+		hmap.put("else", "again");
+		hmap.put("undef", null);
+		HashMap<String, Object> hmapinner = new HashMap<String, Object>();
+		hmapinner.put("one", "one");
+		hmapinner.put("two", "two");
+		hmapinner.put("true", true);
+		hmapinner.put("false", false);
+		hmapinner.put("else", "again");
+		hmapinner.put("undef", null);
+
+		hmap.put("inner", hmapinner);
+	      
+		Object[] inputArray = {
+				Character.MIN_VALUE,
+				Character.MAX_VALUE,
+				Short.MIN_VALUE,
+				Short.MAX_VALUE,
+				Integer.MIN_VALUE,
+				Integer.MAX_VALUE,
+				Long.MIN_VALUE,
+				Long.MAX_VALUE,
+				true,
+				false,
+				null,
+				new byte[] { },
+				new byte[] { '\0', '\1'},
+				"",
+				"test",
+				new String[] {
+						new String("String No 1"),
+						new String("String No 2")
+				},
+				hmap,
+				0.0f,
+				Float.MAX_VALUE,
+				Float.MIN_VALUE,
+				Float.POSITIVE_INFINITY,
+				Float.NEGATIVE_INFINITY,
+				Float.NaN,
+				Double.MAX_VALUE,
+				Double.MIN_VALUE,
+				Double.POSITIVE_INFINITY,
+				Double.NEGATIVE_INFINITY,
+				Double.NaN
+		};  
+
+		ESLIFJSON eslifJSON = null;
+		try {
+			eslifJSON = new ESLIFJSON(eslif);
+			for (int i = 0; i < strings.length; i++) {
+				String string = new String(strings[i]);
+	
+				eslifLogger.info("Testing decode() on " + string);
+				try {
+					Object result = eslifJSON.decode(string);
+				    Describe d = new Describe();
+				    String resultDescribe = d.describe(result);
+					eslifLogger.info("Result: " + resultDescribe);
+				} catch (Exception e) {
+					System.err.println("Failed to parse " + string + ": " + e.getMessage());
+				}
+			}
+
+			for (int i = 0; i < inputArray.length; i++) {
+				String output = eslifJSON.encode(inputArray[i]);
+			    Describe d = new Describe();
+			    String inputDescribe = d.describe(inputArray[i]);
+				eslifLogger.info("Encoding of " + inputDescribe + " : " + output);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		} finally {
+			if (eslifJSON != null) {
+				try {
+					eslifJSON.free();
+				} catch (ESLIFException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
