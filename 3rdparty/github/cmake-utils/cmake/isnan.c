56d6c9b70437f1e1eb5f23859eb620070967ad04
Jean-Damien Durand <jeandamiendurand@free.fr>
2020-06-14 08:14:24 +0200

    Squashed '3rdparty/github/tconv/' changes from 23239076..18225422
    18225422 Version 1.0.63
    520417c7 Merge commit '30fc4c9f0c490607258c0b217af2cd500efe0691'
    30fc4c9f Squashed '3rdparty/github/coveralls-cmake/' changes from 9f96714..0feb9aa
    f1a04042 Merge commit '81fb403dddf022df3fb4e261f537ff2ace8232a7'
    81fb403d Squashed '3rdparty/github/dlfcn-win32/' changes from 974b39c..61ad60f
    929d8c75 Merge commit '8bf23a9537cfbff7fee06831d79cc1024dbb5b30'
    8bf23a95 Squashed '3rdparty/github/cmake-utils/' changes from 2167336..7584b41
    1bb0af30 Version 1.0.62 [ci skip]
    8f3e0b19 Set ICU_DEBUG on cmake stuff on AppVeyor [ci skip]
    e04a3860 Update FindICU.cmake to a pkgconfig aware version
    
    git-subtree-dir: 3rdparty/github/tconv
    git-subtree-split: 18225422116cb14e2ae5160de083a580343501bd

--- 3rdparty/github/cmake-utils/cmake/isnan.c
+++ 3rdparty/github/cmake-utils/cmake/isnan.c
@@ -1,6 +1,10 @@
 #if HAVE_MATH_H
 #include <math.h>
 #endif
+#ifdef HAVE_ISNAN_REPLACEMENT
+#  undef C_ISNAN
+#  define C_ISNAN(x) (__builtin_isnan(x))
+#endif
 int main() {
   short x = C_ISNAN(0.0);
   return 0;
