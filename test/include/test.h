a8aad1301e72e4d3d7ef5936ddafa4396ae85b24
Jean-Damien Durand <jeandamiendurand@free.fr>
2021-02-09 06:55:15 +0100

    Squashed '3rdparty/github/cmake-utils/' changes from 8f86b202..55504f57
    55504f57 Let the user set DL and MATH libs - he knows
    c13838ea Set all policies once
    2606c4a8 Move CMP0075
    1d2b04a6 Add CMP0075
    9971d2c7 Tranversal systematic add of link and m (when possible) libraries
    
    git-subtree-dir: 3rdparty/github/cmake-utils
    git-subtree-split: 55504f57e92404b0de0181d9cbef2b6d97ff06bc

---
+++ test/include/test.h
@@ -0,0 +1,15 @@
+#ifndef CMAKE_UTILS_TEST_H
+#define CMAKE_UTILS_TEST_H
+
+#include <cmake_utils/export.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  cmake_utils_EXPORT double call_sin(double x);
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* CMAKE_UTILS_TEST_H */
