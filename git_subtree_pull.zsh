076bfda931efc8b394854eca161eeb9114d64d8c
Jean-Damien Durand <jeandamiendurand@free.fr>
2021-11-05 01:44:29 +0100

    git_subtree_pull.zsh should work on current branch when doing hard reset [ci skip]

--- git_subtree_pull.zsh
+++ git_subtree_pull.zsh
@@ -15,7 +15,8 @@ foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrap
   git fetch $this master
 }
 
-git reset --hard origin/master
+current_branch=`git branch --show-current`
+git reset --hard origin/$current_branch
 git clean -ffdx
 foreach this (cmake-utils genericLogger genericStack genericHash tconv marpaWrapper luaunpanic lua-template-engine) {
   git subtree pull --prefix 3rdparty/github/$this $this master --squash
