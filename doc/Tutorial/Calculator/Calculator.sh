ff1651f62522eec536aaa4bddfdcf5d1947c1fe9
Jean-Damien Durand <jeandamiendurand@free.fr>
2021-12-04 17:24:36 +0100

    Calculator/Calculator.sh: Fix

--- doc/Tutorial/Calculator/Calculator.sh
+++ doc/Tutorial/Calculator/Calculator.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-pushd /home/jdurand/git/c-marpaESLIF
+pushd ../../..
 make marpaESLIF
 popd
 
-cc -g -I include -I output/include -I 3rdparty/github/genericLogger/include -I 3rdparty/github/genericLogger/output/include -o /tmp/jdd doc/Tutorial/Calculator/Calculator.c -L output/lib -lmarpaESLIF -L 3rdparty/github/genericLogger/output/lib -lgenericLogger -lm && echo "5 / ( 2 * 3)" | LD_LIBRARY_PATH=`pwd`/output/lib:`pwd`/3rdparty/github/genericLogger/output/lib /tmp/jdd
+cc -g -I ../../../include -I ../../../output/include -I ../../../3rdparty/github/genericLogger/include -I ../../../3rdparty/github/genericLogger/output/include -o ./Calculator ./Calculator.c -L ../../../output/lib -lmarpaESLIF -L ../../../3rdparty/github/genericLogger/output/lib -lgenericLogger -lm && echo "5 / ( 2 * 3)" | LD_LIBRARY_PATH=../../../output/lib:../../../3rdparty/github/genericLogger/output/lib ./Calculator
