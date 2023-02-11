45e2bbe1fc8cf8ffae11795ba5d056d10d5b40ac
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-02-11 07:46:28 +0100

    perl: Removed Log4perl dependency

--- src/bindings/perl/etc/callout_inspect.pl
+++ src/bindings/perl/etc/callout_inspect.pl
@@ -33,23 +33,9 @@ sub setResult          { $_[0]->{result} = $_[1] }
 package main;
 use strict;
 use warnings FATAL => 'all';
-use Log::Log4perl qw/:easy/;
-use Log::Any::Adapter;
 use Log::Any qw/$log/;
+use Log::Any::Adapter 'Stdout';
 use MarpaX::ESLIF;
-
-#
-# Init log
-#
-our $defaultLog4perlConf = '
-log4perl.rootLogger              = INFO, Screen
-log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
-log4perl.appender.Screen.stderr  = 0
-log4perl.appender.Screen.layout  = PatternLayout
-log4perl.appender.Screen.layout.ConversionPattern = %d %-5p %6P %m{chomp}%n
-';
-Log::Log4perl::init(\$defaultLog4perlConf);
-Log::Any::Adapter->set('Log4perl');
 
 my $re_as_string = shift || die "Usage: $0 regexp";
 #
