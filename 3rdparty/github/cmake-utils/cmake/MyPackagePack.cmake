d6f5f858ac6885cee5e605449ffc70946fda289c
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-04-22 08:50:45 +0200

    Squashed '3rdparty/github/genericHash/' changes from e0c86b3a1..6fbbd9572
    6fbbd9572 Merge commit '0f56f7bc5c2ee717387ab4a4e261e3249982f3ab'
    0f56f7bc5 Squashed '3rdparty/github/genericStack/' changes from c075d1d..c4ed625
    9a1448e4c CMakeLists.txt: genericHash exports nothing (no library)
    c87914948 CMakeLists.txt: Add exports
    6e4977268 Squashed '3rdparty/github/genericStack/' changes from 7bca012..c075d1d
    e80e440ba Merge commit '6e49772688bfddc5c533cefd63507ab8dab88be1'
    bd66b12b7 Merge commit '9dd675ff71475866bf68c490b7fac7a6c5821e5a'
    9dd675ff7 Squashed '3rdparty/github/genericLogger/' changes from 34d13c5..767bd1c
    71f7e35ce .appveyor.yml: use cpack instead of nmake package
    65300884d .appveyor.yml: Add VS2022
    6609be0fc workflows: use cpack explicitly
    d4a91ae7a workflows: add package
    bea38e936 README.pod: update
    90b710675 .github workflows
    6d6a077f7 CMakeLists.txt: CMake minimum version 3.15
    81a20e18a Squashed '3rdparty/github/genericStack/' changes from 155e371..7bca012
    e25d9e8a7 Merge commit '81a20e18ac2fa5614a6c4480e942f35c5a32e0bc'
    f0c702f00 Squashed '3rdparty/github/genericLogger/' changes from 2ecceeb..34d13c5
    096c5dc5c Merge commit 'f0c702f00b0deb9f0b112cdf0f958337387a6bef'
    f2ad5c806 Merge commit '767770e1820c69ac015ff36cbf1d3ccc08a7e8f5'
    767770e18 Squashed '3rdparty/github/cmake-utils/' changes from 0dac9cd..8b5b753
    b2d5de64f Squashed '3rdparty/github/genericLogger/' changes from 1de89ed..2ecceeb
    cbcaad25a Merge commit 'b2d5de64f83c03d923de65682016176c4423240f'
    de2361a45 .appveyor.yml: Stop building on VS <= 2013 (workers have a too old CMake)
    c99b75b84 CMakeLists.txt: dev+
    c28b50f43 Squashed '3rdparty/github/genericStack/' changes from 2315f48..155e371
    a9df16c91 Merge commit 'c28b50f434d469984cb05202e43b52d5aba9ea39'
    d7a278b04 Squashed '3rdparty/github/genericLogger/' changes from 3db8952..1de89ed
    d14d88f81 Merge commit 'd7a278b04f7c7bc346827a595786545885213acc'
    70310e404 Squashed '3rdparty/github/cmake-utils/' changes from 5c4325b..0dac9cd
    d2dd34c28 Merge commit '70310e40437266968f7139047b48a1218b0a6fdb'
    
    git-subtree-dir: 3rdparty/github/genericHash
    git-subtree-split: 6fbbd957211e689c76c82cfaf5a8dbc2c359ff12

--- 3rdparty/github/cmake-utils/cmake/MyPackagePack.cmake
+++ 3rdparty/github/cmake-utils/cmake/MyPackagePack.cmake
@@ -9,16 +9,16 @@ MACRO (MYPACKAGEPACK VENDOR SUMMARY)
     IF (MYPACKAGE_DEBUG)
       MESSAGE (STATUS "[${PROJECT_NAME}-BOOTSTRAP-DEBUG] Configuration CPack")
     ENDIF ()
-    SET (CPACK_PACKAGE_NAME                "${PROJECT_NAME}")
-    SET (CPACK_PACKAGE_VENDOR              "${VENDOR}")
-    SET (CPACK_PACKAGE_DESCRIPTION_SUMMARY "${SUMMARY}")
-    SET (CPACK_PACKAGE_VERSION             "${${PROJECT_NAME}_VERSION}")
-    SET (CPACK_PACKAGE_VERSION_MAJOR       "${${PROJECT_NAME}_VERSION_MAJOR}")
-    SET (CPACK_PACKAGE_VERSION_MINOR       "${${PROJECT_NAME}_VERSION_MINOR}")
-    SET (CPACK_PACKAGE_VERSION_PATCH       "${${PROJECT_NAME}_VERSION_PATCH}")
-    IF (EXISTS "${PROJECT_SOURCE_DIR}/LICENSE")
-      CONFIGURE_FILE ("${PROJECT_SOURCE_DIR}/LICENSE"  "${CMAKE_CURRENT_BINARY_DIR}/LICENSE.txt")
-      SET (CPACK_RESOURCE_FILE_LICENSE     "${CMAKE_CURRENT_BINARY_DIR}/LICENSE.txt")
+    SET (CPACK_PACKAGE_NAME                ${PROJECT_NAME})
+    SET (CPACK_PACKAGE_VENDOR              ${VENDOR})
+    SET (CPACK_PACKAGE_DESCRIPTION_SUMMARY ${SUMMARY})
+    SET (CPACK_PACKAGE_VERSION             ${${PROJECT_NAME}_VERSION})
+    SET (CPACK_PACKAGE_VERSION_MAJOR       ${${PROJECT_NAME}_VERSION_MAJOR})
+    SET (CPACK_PACKAGE_VERSION_MINOR       ${${PROJECT_NAME}_VERSION_MINOR})
+    SET (CPACK_PACKAGE_VERSION_PATCH       ${${PROJECT_NAME}_VERSION_PATCH})
+    IF (EXISTS ${PROJECT_SOURCE_DIR}/LICENSE)
+      CONFIGURE_FILE (${PROJECT_SOURCE_DIR}/LICENSE ${CMAKE_CURRENT_BINARY_DIR}/LICENSE.txt)
+      SET (CPACK_RESOURCE_FILE_LICENSE     ${CMAKE_CURRENT_BINARY_DIR}/LICENSE.txt)
     ELSE ()
       IF (MYPACKAGE_DEBUG)
         MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] No LICENSE")
@@ -30,187 +30,121 @@ MACRO (MYPACKAGEPACK VENDOR SUMMARY)
     #
     SET (CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)
     #
+    # And explicit show them
+    #
+    SET (CPACK_MONOLITHIC_INSTALL FALSE)
+    #
     # Always enable archive
     #
     SET (CPACK_ARCHIVE_COMPONENT_INSTALL ON)
     #
-    # We want to recuperate some cmake variables to do a temporary install
-    # and the only way with via the CPackProperties.cmake file.
-    # This file must contain only CPack compatible commands -;
+    # We need a way to know if make install is running under CPACK or not
     #
-    # Any variable starting with CPACK_ is exported to cpack...
-    #
-    SET (CPACK_PROJECT_SOURCE_DIR ${PROJECT_SOURCE_DIR})
-    SET (CPACK_PROJECT_CONFIG_FILE "${PROJECT_SOURCE_DIR}/CPackCustomProjectConfig.cmake")
-    SET (CPACK_CMAKE_MAKE_PROGRAM ${CMAKE_MAKE_PROGRAM})
-    SET (CPACK_TEMP_BASENAME "CpackTempDestdir")
-    SET (CPACK_TEMP_DIRNAME ${PROJECT_SOURCE_DIR})
-    SET (CPACK_TEMP_FULLPATH "${CPACK_TEMP_DIRNAME}/${CPACK_TEMP_BASENAME}")
-    #
-    # Generate CPACK_PROJECT_CONFIG_FILE
-    #
-    IF (EXISTS ${CPACK_PROJECT_CONFIG_FILE})
-      MESSAGE (STATUS "Removing ${CPACK_PROJECT_CONFIG_FILE}")
-      FILE (REMOVE ${CPACK_PROJECT_CONFIG_FILE})
-    ENDIF ()
-    MESSAGE (STATUS "Generating ${CPACK_PROJECT_CONFIG_FILE}")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "MESSAGE (STATUS \"Removing \${CPACK_TEMP_FULLPATH}\")\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "FILE (REMOVE_RECURSE \${CPACK_TEMP_FULLPATH})\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "MESSAGE (STATUS \"Installing to \${CPACK_TEMP_FULLPATH}\")\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "EXECUTE_PROCESS (COMMAND \${CPACK_CMAKE_MAKE_PROGRAM} install DESTDIR=\${CPACK_TEMP_BASENAME} WORKING_DIRECTORY \${CPACK_TEMP_DIRNAME})\n")
-    #
-    # Rework internal variables to avoid zombies
-    #
-    GET_CMAKE_PROPERTY(COMPONENTS _components)
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "SET (_component_ok_list)\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "FOREACH (_component IN LISTS CPACK_COMPONENTS_ALL)\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "  SET (_install_manifest__path \"\${CPACK_PROJECT_SOURCE_DIR}/install_manifest_\${_component}.txt\")\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "  IF (EXISTS \${_install_manifest__path})\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "    FILE( READ \${_install_manifest__path} _content HEX)\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "    STRING (LENGTH \"\${_content}\" _content_length)\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "    IF (\${_content_length} GREATER 0)\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "      LIST (APPEND _component_ok_list \${_component})\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "    ENDIF ()\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "  ENDIF ()\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "ENDFOREACH ()\n")
-    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE} "SET (CPACK_COMPONENTS_ALL \"\${_component_ok_list}\")\n")
+    SET (CPACK_PROJECT_CONFIG_FILE_PATH ${CMAKE_CURRENT_BINARY_DIR}/cpack_project_config_file.cmake)
+    FILE (WRITE ${CPACK_PROJECT_CONFIG_FILE_PATH} "message(STATUS \"Setting ENV{CPACK_IS_RUNNING}\")\n")
+    FILE (APPEND ${CPACK_PROJECT_CONFIG_FILE_PATH} "set(ENV{CPACK_IS_RUNNING} TRUE)\n")
+    SET (CPACK_PROJECT_CONFIG_FILE ${CPACK_PROJECT_CONFIG_FILE_PATH})
     #
     # Include CPack - from now on we will have access to CPACK own macros
     #
     INCLUDE (CPack)
     #
-    # Install types
-    #
-    IF (_HAVE_HEADERCOMPONENT OR _HAVE_DYNAMICLIBRARYCOMPONENT OR _HAVE_STATICLIBRARYCOMPONENT OR _HAVE_APPLICATIONCOMPONENT OR _HAVE_MANPAGECOMPONENT)
-      SET (_CAN_FULLTYPE TRUE)
-    ELSE ()
-      SET (_CAN_FULLTYPE FALSE)
-    ENDIF ()
-    IF (_HAVE_HEADERCOMPONENT OR _HAVE_DYNAMICLIBRARYCOMPONENT OR _HAVE_STATICLIBRARYCOMPONENT)
-      SET (_CAN_DEVELOPMENTTYPE TRUE)
-    ELSE ()
-      SET (_CAN_DEVELOPMENTTYPE FALSE)
-    ENDIF ()
-    IF (MYPACKAGE_DEBUG)
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Full install type        : ${_CAN_FULLTYPE}")
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Development install type : ${_CAN_DEVELOPMENTTYPE}")
-    ENDIF ()
-    IF (_CAN_FULLTYPE)
-      IF (MYPACKAGE_DEBUG)
-        MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add FullType")
-      ENDIF ()
-      CPACK_ADD_INSTALL_TYPE (FullType        DISPLAY_NAME "Full")
-    ENDIF ()
-    IF (_CAN_DEVELOPMENTTYPE)
-      IF (MYPACKAGE_DEBUG)
-        MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add DevelopmentType")
-      ENDIF ()
-      CPACK_ADD_INSTALL_TYPE (DevelopmentType DISPLAY_NAME "Development")
-    ENDIF ()
-    #
     # Groups
     #
-    IF (_HAVE_HEADERCOMPONENT)
+    IF (${PROJECT_NAME}_HAVE_HEADERCOMPONENT)
       SET (_CAN_DEVELOPMENTGROUP TRUE)
     ELSE ()
       SET (_CAN_DEVELOPMENTGROUP FALSE)
     ENDIF ()
-    IF (_HAVE_DYNAMICLIBRARYCOMPONENT OR _HAVE_STATICLIBRARYCOMPONENT)
-      SET (_CAN_DEVELOPMENTLIBRARYGROUP TRUE)
+    IF (${PROJECT_NAME}_HAVE_LIBRARYCOMPONENT)
+      SET (_CAN_LIBRARYGROUP TRUE)
     ELSE ()
-      SET (_CAN_DEVELOPMENTLIBRARYGROUP FALSE)
+      SET (_CAN_LIBRARYGROUP FALSE)
     ENDIF ()
-    IF (_HAVE_MANPAGECOMPONENT)
+    IF (${PROJECT_NAME}_HAVE_MANPAGECOMPONENT)
       SET (_CAN_DOCUMENTGROUP TRUE)
     ELSE ()
       SET (_CAN_DOCUMENTGROUP FALSE)
     ENDIF ()
-    IF (_HAVE_APPLICATIONCOMPONENT)
+    IF (${PROJECT_NAME}_HAVE_APPLICATIONCOMPONENT)
       SET (_CAN_RUNTIMEGROUP TRUE)
     ELSE ()
       SET (_CAN_RUNTIMEGROUP FALSE)
     ENDIF ()
     IF (MYPACKAGE_DEBUG)
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Development group        : ${_CAN_DEVELOPMENTGROUP}")
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Development Library group: ${_CAN_DEVELOPMENTLIBRARYGROUP}")
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Document group           : ${_CAN_DOCUMENTGROUP}")
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Runtime group            : ${_CAN_RUNTIMEGROUP}")
+      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Development group : ${_CAN_DEVELOPMENTGROUP}")
+      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Library group     : ${_CAN_LIBRARYGROUP}")
+      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Document group    : ${_CAN_DOCUMENTGROUP}")
+      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Runtime group     : ${_CAN_RUNTIMEGROUP}")
     ENDIF ()
     IF (_CAN_DEVELOPMENTGROUP)
       IF (MYPACKAGE_DEBUG)
         MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add DevelopmentGroup")
       ENDIF ()
-      CPACK_ADD_COMPONENT_GROUP (DevelopmentGroup        DISPLAY_NAME "Development" DESCRIPTION "Develoment files\n\nThis group contains libraries and headers"  EXPANDED)
+      CPACK_ADD_COMPONENT_GROUP (DevelopmentGroup        DISPLAY_NAME "Development" DESCRIPTION "Development\n\nContains header and CMake components"  EXPANDED)
     ENDIF ()
-    IF (_CAN_DEVELOPMENTLIBRARYGROUP)
+    IF (_CAN_LIBRARYGROUP)
       IF (MYPACKAGE_DEBUG)
-        MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add DevelopmentLibraryGroup")
+        MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add LibraryGroup")
       ENDIF ()
-      CPACK_ADD_COMPONENT_GROUP (DevelopmentLibraryGroup DISPLAY_NAME "Libraries"   DESCRIPTION "Library files\n\nBoth static and dynamic version are provided" PARENT_GROUP DevelopmentGroup)
+      CPACK_ADD_COMPONENT_GROUP (LibraryGroup DISPLAY_NAME "Libraries"   DESCRIPTION "Libraries\n\nContains dynamic and static components" EXPANDED)
     ENDIF ()
     IF (_CAN_DOCUMENTGROUP)
       IF (MYPACKAGE_DEBUG)
-        MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add DevelopmentLibraryGroup")
+        MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add DocumentGroup")
       ENDIF ()
-      CPACK_ADD_COMPONENT_GROUP (DocumentGroup           DISPLAY_NAME "Documents"   DESCRIPTION "Document files\n\nThis group contains all the provided documentation" EXPANDED)
+      CPACK_ADD_COMPONENT_GROUP (DocumentGroup           DISPLAY_NAME "Documents"   DESCRIPTION "Documents\n\nContains manpages component" EXPANDED)
     ENDIF ()
     IF (_CAN_RUNTIMEGROUP)
       IF (MYPACKAGE_DEBUG)
         MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add RuntimeGroup")
       ENDIF ()
-      CPACK_ADD_COMPONENT_GROUP (RuntimeGroup            DISPLAY_NAME "Runtime"     DESCRIPTION "Runtime applications"  EXPANDED)
+      CPACK_ADD_COMPONENT_GROUP (RuntimeGroup            DISPLAY_NAME "Runtime"     DESCRIPTION "Runtime\n\nContains application component"  EXPANDED)
     ENDIF ()
     #
     # Components
     #
+    SET (CPACK_COMPONENTS_ALL)
     IF (MYPACKAGE_DEBUG)
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Manpage component        : ${_HAVE_MANPAGECOMPONENT}")
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Dynamic Library component: ${_HAVE_DYNAMICLIBRARYCOMPONENT}")
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Static Library component : ${_HAVE_STATICLIBRARYCOMPONENT}")
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Header component         : ${_HAVE_HEADERCOMPONENT}")
-      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Application component    : ${_HAVE_APPLICATIONCOMPONENT}")
+      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Manpage component        : ${${PROJECT_NAME}_HAVE_MANPAGECOMPONENT}")
+      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Library component        : ${${PROJECT_NAME}_HAVE_LIBRARYCOMPONENT}")
+      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Header component         : ${${PROJECT_NAME}_HAVE_HEADERCOMPONENT}")
+      MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] Application component    : ${${PROJECT_NAME}_HAVE_APPLICATIONCOMPONENT}")
     ENDIF ()
-    IF (_HAVE_MANPAGECOMPONENT)
+    IF (${PROJECT_NAME}_HAVE_MANPAGECOMPONENT)
       IF (MYPACKAGE_DEBUG)
         MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add ManpageComponent")
       ENDIF ()
       CPACK_ADD_COMPONENT(ManpageComponent
                           DISPLAY_NAME "Man pages"
-                          DESCRIPTION "Documentation in the man format\n\nUseful on all platforms but Windows, in general"
+                          DESCRIPTION "Documentation in the man format"
                           GROUP DocumentGroup
-                          INSTALL_TYPES FullType)
+                          )
+      LIST (APPEND CPACK_COMPONENTS_ALL ManpageComponent)
     ENDIF ()
-    IF (_HAVE_DYNAMICLIBRARYCOMPONENT)
+    IF (${PROJECT_NAME}_HAVE_LIBRARYCOMPONENT)
       IF (MYPACKAGE_DEBUG)
-        MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add DynamicLibraryComponent")
+        MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add LibraryComponent")
       ENDIF ()
-      CPACK_ADD_COMPONENT(DynamicLibraryComponent
-                          DISPLAY_NAME "Dynamic"
-                          DESCRIPTION "Dynamic Libraries\n\nNecessary almost anytime"
-                          GROUP DevelopmentLibraryGroup
-                          INSTALL_TYPES FullType DevelopmentType)
+      CPACK_ADD_COMPONENT(LibraryComponent
+                          DISPLAY_NAME "Libraries"
+                          DESCRIPTION "Dynamic and Static Libraries"
+                          GROUP LibraryGroup
+                          )
+      LIST (APPEND CPACK_COMPONENTS_ALL LibraryComponent)
     ENDIF ()
-    IF (_HAVE_STATICLIBRARYCOMPONENT)
-      IF (MYPACKAGE_DEBUG)
-        MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add StaticLibraryComponent")
-      ENDIF ()
-      CPACK_ADD_COMPONENT(StaticLibraryComponent
-                          DISPLAY_NAME "Static"
-                          DESCRIPTION "Static Libraries\n\nOnly programmers would eventually need that"
-                          GROUP DevelopmentLibraryGroup
-                          INSTALL_TYPES FullType DevelopmentType)
-    ENDIF ()
-    IF (_HAVE_HEADERCOMPONENT)
+    IF (${PROJECT_NAME}_HAVE_HEADERCOMPONENT)
       IF (MYPACKAGE_DEBUG)
         MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add HeaderComponent")
       ENDIF ()
       CPACK_ADD_COMPONENT(HeaderComponent
                           DISPLAY_NAME "Headers"
-                          DESCRIPTION "C/C++ Headers\n\nProgrammers will need these files"
+                          DESCRIPTION "C/C++ Headers"
                           GROUP DevelopmentGroup
-                          INSTALL_TYPES FullType DevelopmentType)
+                          )
+      LIST (APPEND CPACK_COMPONENTS_ALL HeaderComponent)
     ENDIF ()
-    IF (_HAVE_APPLICATIONCOMPONENT)
+    IF (${PROJECT_NAME}_HAVE_APPLICATIONCOMPONENT)
       IF (MYPACKAGE_DEBUG)
         MESSAGE (STATUS "[${PROJECT_NAME}-PACK-DEBUG] ... Add ApplicationComponent")
       ENDIF ()
@@ -218,20 +152,8 @@ MACRO (MYPACKAGEPACK VENDOR SUMMARY)
                           DISPLAY_NAME "Applications"
                           DESCRIPTION "Executables"
                           GROUP RuntimeGroup
-                          INSTALL_TYPES FullType
-                          DEPENDS DynamicLibraryComponent)
+                          DEPENDS LibraryComponent)
+      LIST (APPEND CPACK_COMPONENTS_ALL ApplicationComponent)
     ENDIF ()
-    #
-    # Quite subtil, but the "package" target is not visible at this time. There is a old standing bug
-    # in CMake about this.
-    # We use a custom target for package generation.
-    # In addition man pages is generated using the INSTALL(CODE xxx) hack.
-    #
-    ADD_CUSTOM_TARGET(pack
-      COMMAND ${CMAKE_MAKE_PROGRAM} package
-      COMMENT "Packaging ${PROJECT_NAME}"
-	  DEPENDS man
-      VERBATIM
-      )
   ENDIF ()		    
 ENDMACRO()
