7e3b1defa8e61ca246c25cd5c4d0737a5d06d916
Jean-Damien Durand <jeandamiendurand@free.fr>
2023-04-30 08:46:21 +0200

    Squashed '3rdparty/github/luaunpanic/' changes from 33a00a7f8..e8b6f3087
    e8b6f3087 src/lundump.c: Prevent collision with Windows natives (LoadString in particular)
    0771e33c7 Revert "src/lprefix.h: NOAPISET on Windows"
    7733f5209 src/lprefix.h: NOAPISET on Windows
    05783169c src/luaunpanic_amalgamation.c: Add
    f6981e122 Prepare for amalgamation and static embed
    
    git-subtree-dir: 3rdparty/github/luaunpanic
    git-subtree-split: e8b6f30871ca726673cbde08c9bca3a8236c17d5

--- src/lundump.c
+++ src/lundump.c
@@ -43,104 +43,104 @@ static l_noret error(LoadState *S, const char *why) {
 
 
 /*
-** All high-level loads go through LoadVector; you can change it to
+** All high-level loads go through lundump_LoadVector; you can change it to
 ** adapt to the endianness of the input
 */
-#define LoadVector(S,b,n)	LoadBlock(S,b,(n)*sizeof((b)[0]))
+#define lundump_LoadVector(S,b,n)	lundump_LoadBlock(S,b,(n)*sizeof((b)[0]))
 
-static void LoadBlock (LoadState *S, void *b, size_t size) {
+static void lundump_LoadBlock (LoadState *S, void *b, size_t size) {
   if (luaZ_read(S->Z, b, size) != 0)
     error(S, "truncated");
 }
 
 
-#define LoadVar(S,x)		LoadVector(S,&x,1)
+#define lundump_LoadVar(S,x)		lundump_LoadVector(S,&x,1)
 
 
-static lu_byte LoadByte (LoadState *S) {
+static lu_byte lundump_LoadByte (LoadState *S) {
   lu_byte x;
-  LoadVar(S, x);
+  lundump_LoadVar(S, x);
   return x;
 }
 
 
-static int LoadInt (LoadState *S) {
+static int lundump_LoadInt (LoadState *S) {
   int x;
-  LoadVar(S, x);
+  lundump_LoadVar(S, x);
   return x;
 }
 
 
-static lua_Number LoadNumber (LoadState *S) {
+static lua_Number lundump_LoadNumber (LoadState *S) {
   lua_Number x;
-  LoadVar(S, x);
+  lundump_LoadVar(S, x);
   return x;
 }
 
 
-static lua_Integer LoadInteger (LoadState *S) {
+static lua_Integer lundump_LoadInteger (LoadState *S) {
   lua_Integer x;
-  LoadVar(S, x);
+  lundump_LoadVar(S, x);
   return x;
 }
 
 
-static TString *LoadString (LoadState *S) {
-  size_t size = LoadByte(S);
+static TString *lundump_LoadString (LoadState *S) {
+  size_t size = lundump_LoadByte(S);
   if (size == 0xFF)
-    LoadVar(S, size);
+    lundump_LoadVar(S, size);
   if (size == 0)
     return NULL;
   else if (--size <= LUAI_MAXSHORTLEN) {  /* short string? */
     char buff[LUAI_MAXSHORTLEN];
-    LoadVector(S, buff, size);
+    lundump_LoadVector(S, buff, size);
     return luaS_newlstr(S->L, buff, size);
   }
   else {  /* long string */
     TString *ts = luaS_createlngstrobj(S->L, size);
-    LoadVector(S, getstr(ts), size);  /* load directly in final place */
+    lundump_LoadVector(S, getstr(ts), size);  /* load directly in final place */
     return ts;
   }
 }
 
 
-static void LoadCode (LoadState *S, Proto *f) {
-  int n = LoadInt(S);
+static void lundump_LoadCode (LoadState *S, Proto *f) {
+  int n = lundump_LoadInt(S);
   f->code = luaM_newvector(S->L, n, Instruction);
   f->sizecode = n;
-  LoadVector(S, f->code, n);
+  lundump_LoadVector(S, f->code, n);
 }
 
 
-static void LoadFunction(LoadState *S, Proto *f, TString *psource);
+static void lundump_LoadFunction(LoadState *S, Proto *f, TString *psource);
 
 
-static void LoadConstants (LoadState *S, Proto *f) {
+static void lundump_LoadConstants (LoadState *S, Proto *f) {
   int i;
-  int n = LoadInt(S);
+  int n = lundump_LoadInt(S);
   f->k = luaM_newvector(S->L, n, TValue);
   f->sizek = n;
   for (i = 0; i < n; i++)
     setnilvalue(&f->k[i]);
   for (i = 0; i < n; i++) {
     TValue *o = &f->k[i];
-    int t = LoadByte(S);
+    int t = lundump_LoadByte(S);
     switch (t) {
     case LUA_TNIL:
       setnilvalue(o);
       break;
     case LUA_TBOOLEAN:
-      setbvalue(o, LoadByte(S));
+      setbvalue(o, lundump_LoadByte(S));
       break;
     case LUA_TNUMFLT:
-      setfltvalue(o, LoadNumber(S));
+      setfltvalue(o, lundump_LoadNumber(S));
       break;
     case LUA_TNUMINT:
-      setivalue(o, LoadInteger(S));
+      setivalue(o, lundump_LoadInteger(S));
       break;
     case LUA_TSHRSTR:
     case LUA_TLNGSTR:
-      setsvalue2n(S->L, o, LoadString(S));
+      setsvalue2n(S->L, o, lundump_LoadString(S));
       break;
     default:
       lua_assert(0);
@@ -149,84 +149,84 @@ static void LoadConstants (LoadState *S, Proto *f) {
 }
 
 
-static void LoadProtos (LoadState *S, Proto *f) {
+static void lundump_LoadProtos (LoadState *S, Proto *f) {
   int i;
-  int n = LoadInt(S);
+  int n = lundump_LoadInt(S);
   f->p = luaM_newvector(S->L, n, Proto *);
   f->sizep = n;
   for (i = 0; i < n; i++)
     f->p[i] = NULL;
   for (i = 0; i < n; i++) {
     f->p[i] = luaF_newproto(S->L);
-    LoadFunction(S, f->p[i], f->source);
+    lundump_LoadFunction(S, f->p[i], f->source);
   }
 }
 
 
-static void LoadUpvalues (LoadState *S, Proto *f) {
+static void lundump_LoadUpvalues (LoadState *S, Proto *f) {
   int i, n;
-  n = LoadInt(S);
+  n = lundump_LoadInt(S);
   f->upvalues = luaM_newvector(S->L, n, Upvaldesc);
   f->sizeupvalues = n;
   for (i = 0; i < n; i++)
     f->upvalues[i].name = NULL;
   for (i = 0; i < n; i++) {
-    f->upvalues[i].instack = LoadByte(S);
-    f->upvalues[i].idx = LoadByte(S);
+    f->upvalues[i].instack = lundump_LoadByte(S);
+    f->upvalues[i].idx = lundump_LoadByte(S);
   }
 }
 
 
-static void LoadDebug (LoadState *S, Proto *f) {
+static void lundump_LoadDebug (LoadState *S, Proto *f) {
   int i, n;
-  n = LoadInt(S);
+  n = lundump_LoadInt(S);
   f->lineinfo = luaM_newvector(S->L, n, int);
   f->sizelineinfo = n;
-  LoadVector(S, f->lineinfo, n);
-  n = LoadInt(S);
+  lundump_LoadVector(S, f->lineinfo, n);
+  n = lundump_LoadInt(S);
   f->locvars = luaM_newvector(S->L, n, LocVar);
   f->sizelocvars = n;
   for (i = 0; i < n; i++)
     f->locvars[i].varname = NULL;
   for (i = 0; i < n; i++) {
-    f->locvars[i].varname = LoadString(S);
-    f->locvars[i].startpc = LoadInt(S);
-    f->locvars[i].endpc = LoadInt(S);
+    f->locvars[i].varname = lundump_LoadString(S);
+    f->locvars[i].startpc = lundump_LoadInt(S);
+    f->locvars[i].endpc = lundump_LoadInt(S);
   }
-  n = LoadInt(S);
+  n = lundump_LoadInt(S);
   for (i = 0; i < n; i++)
-    f->upvalues[i].name = LoadString(S);
+    f->upvalues[i].name = lundump_LoadString(S);
 }
 
 
-static void LoadFunction (LoadState *S, Proto *f, TString *psource) {
-  f->source = LoadString(S);
+static void lundump_LoadFunction (LoadState *S, Proto *f, TString *psource) {
+  f->source = lundump_LoadString(S);
   if (f->source == NULL)  /* no source in dump? */
     f->source = psource;  /* reuse parent's source */
-  f->linedefined = LoadInt(S);
-  f->lastlinedefined = LoadInt(S);
-  f->numparams = LoadByte(S);
-  f->is_vararg = LoadByte(S);
-  f->maxstacksize = LoadByte(S);
-  LoadCode(S, f);
-  LoadConstants(S, f);
-  LoadUpvalues(S, f);
-  LoadProtos(S, f);
-  LoadDebug(S, f);
+  f->linedefined = lundump_LoadInt(S);
+  f->lastlinedefined = lundump_LoadInt(S);
+  f->numparams = lundump_LoadByte(S);
+  f->is_vararg = lundump_LoadByte(S);
+  f->maxstacksize = lundump_LoadByte(S);
+  lundump_LoadCode(S, f);
+  lundump_LoadConstants(S, f);
+  lundump_LoadUpvalues(S, f);
+  lundump_LoadProtos(S, f);
+  lundump_LoadDebug(S, f);
 }
 
 
 static void checkliteral (LoadState *S, const char *s, const char *msg) {
   char buff[sizeof(LUA_SIGNATURE) + sizeof(LUAC_DATA)]; /* larger than both */
   size_t len = strlen(s);
-  LoadVector(S, buff, len);
+  lundump_LoadVector(S, buff, len);
   if (memcmp(s, buff, len) != 0)
     error(S, msg);
 }
 
 
 static void fchecksize (LoadState *S, size_t size, const char *tname) {
-  if (LoadByte(S) != size)
+  if (lundump_LoadByte(S) != size)
     error(S, luaO_pushfstring(S->L, "%s size mismatch in", tname));
 }
 
@@ -235,9 +235,9 @@ static void fchecksize (LoadState *S, size_t size, const char *tname) {
 
 static void checkHeader (LoadState *S) {
   checkliteral(S, LUA_SIGNATURE + 1, "not a");  /* 1st char already checked */
-  if (LoadByte(S) != LUAC_VERSION)
+  if (lundump_LoadByte(S) != LUAC_VERSION)
     error(S, "version mismatch in");
-  if (LoadByte(S) != LUAC_FORMAT)
+  if (lundump_LoadByte(S) != LUAC_FORMAT)
     error(S, "format mismatch in");
   checkliteral(S, LUAC_DATA, "corrupted");
   checksize(S, int);
@@ -245,9 +245,9 @@ static void checkHeader (LoadState *S) {
   checksize(S, Instruction);
   checksize(S, lua_Integer);
   checksize(S, lua_Number);
-  if (LoadInteger(S) != LUAC_INT)
+  if (lundump_LoadInteger(S) != LUAC_INT)
     error(S, "endianness mismatch in");
-  if (LoadNumber(S) != LUAC_NUM)
+  if (lundump_LoadNumber(S) != LUAC_NUM)
     error(S, "float format mismatch in");
 }
 
@@ -267,11 +267,11 @@ LClosure *luaU_undump(lua_State *L, ZIO *Z, const char *name) {
   S.L = L;
   S.Z = Z;
   checkHeader(&S);
-  cl = luaF_newLclosure(L, LoadByte(&S));
+  cl = luaF_newLclosure(L, lundump_LoadByte(&S));
   setclLvalue(L, L->top, cl);
   luaD_inctop(L);
   cl->p = luaF_newproto(L);
-  LoadFunction(&S, cl->p, NULL);
+  lundump_LoadFunction(&S, cl->p, NULL);
   lua_assert(cl->nupvalues == cl->p->sizeupvalues);
   luai_verifycode(L, buff, cl->p);
   return cl;
